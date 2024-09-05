065b420b141ef69412df5beb02009e38c1553b7d
adrianomts7 <161032614+adrianomts7@users.noreply.github.com>
2024-09-05 19:18:54 -0300

    Novas Mudanças, Mais exercicios

---
+++ Front-End/index.js
@@ -0,0 +1,96 @@
+const tela = document.querySelector("h1")
+tela.innerHTML = ""
+
+
+let data = new Date()
+let dia = data.getDate()
+let mes = data.getMonth()
+let ano = data.getFullYear()
+let hora = data.getHours()
+let minutos = data.getMinutes()
+
+function adicionarZero(num){
+    return num > 10 ? num : `0${num}`
+}
+
+function mesTexto(mes){
+    let mesTexto
+    
+    switch (mes){
+        case 0:
+            mesTexto = "Janeiro"
+            break
+        case 1:
+            mesTexto = "Fevereiro"
+            break
+        case 2:
+            mesTexto = "Março"
+            break
+        case 3:
+            mesTexto = "Abril"
+            break
+        case 4:
+            mesTexto = "Maio"
+            break
+        case 5:
+            mesTexto = "Junho"
+            break
+        case 6:
+            mesTexto = "Julho"
+            break
+        case 7:
+            mesTexto = "Agosto"
+            break
+        case 8:
+            mesTexto = "Setembro"
+            break
+        case 9:
+            mesTexto = "Outubro"
+            break
+        case 10:
+            mesTexto = "Novembro"
+            break
+        case 11:
+            mesTexto = "Dezembro"
+            break
+        default:
+            mesTexto = "Mês invalido"
+    }
+
+    return mesTexto
+
+}
+
+function diaTexto(dia){
+    let diaTexto
+
+    switch (dia){
+        case 0:
+            diaTexto = "Domingo"
+            break
+        case 1:
+            diaTexto = "Segunda-Feira"
+            break
+        case 2:
+            diaTexto = "Terça-Feira"
+            break
+        case 3:
+            diaTexto = "Quarta-Feira"
+            break
+        case 4:
+            diaTexto = "Quinta-feira"
+            break
+        case 5:
+            diaTexto = "Sexta-Feira"
+            break
+        case 6:
+            diaTexto = "Sábado"
+            break
+        default:
+            diaTexto = "Dia invalido"
+    }
+    return diaTexto
+}
+
+tela.innerHTML += `${diaTexto(dia)}, ${dia} de ${mesTexto(mes)} de ${ano} <br/>`
+tela.innerHTML += `${hora}:${adicionarZero(minutos)}`
