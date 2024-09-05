b9450f19905e856d964bebe84bd9ddcbf240c11d
adrianomts7 <161032614+adrianomts7@users.noreply.github.com>
2024-09-05 17:05:36 -0300

    Datas e Horas

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,4 +1,16 @@
+/*
 const tresHoras = 60 * 60 * 3 * 1000
 const umDia = 60 * 60 * 24 * 1000
 const data = new Date(0 + tresHoras + umDia)
+*/
+const data = new Date('2019-04-20 20:59')
+console.log('Dia', data.getDate())
+console.log('Mês', data.getMonth())
+console.log('Ano', data.getFullYear())
+console.log('Hora', data.getHours())
+console.log('Min', data.getMinutes())
+console.log('Seg', data.getSeconds())
+console.log('ms', data.getMilliseconds())
+console.log('Dia Semana', data.getDay()) // 0 - Domingo, 6 - Sábado
 console.log(data.toString())
+console.log(Date.now());
