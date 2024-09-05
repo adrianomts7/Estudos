0fdb19006be299f251cc12b1b9ae49b881fcc1d3
adrianomts7 <161032614+adrianomts7@users.noreply.github.com>
2024-09-05 17:19:25 -0300

    Objeto Date

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,16 +1,18 @@
-/*
-const tresHoras = 60 * 60 * 3 * 1000
-const umDia = 60 * 60 * 24 * 1000
-const data = new Date(0 + tresHoras + umDia)
-*/
-const data = new Date('2019-04-20 20:59')
-console.log('Dia', data.getDate())
-console.log('Mês', data.getMonth())
-console.log('Ano', data.getFullYear())
-console.log('Hora', data.getHours())
-console.log('Min', data.getMinutes())
-console.log('Seg', data.getSeconds())
-console.log('ms', data.getMilliseconds())
-console.log('Dia Semana', data.getDay()) // 0 - Domingo, 6 - Sábado
-console.log(data.toString())
-console.log(Date.now());
+function zeroAesquerda(num){
+    return num >= 10 ? num : `0${num}`
+}
+
+function formatarData(data){
+    const dia = zeroAesquerda(data.getDate())
+    const mes = zeroAesquerda(data.getMonth() + 1)
+    const ano = zeroAesquerda(data.getFullYear())
+    const hora = zeroAesquerda(data.getHours())
+    const min = zeroAesquerda(data.getMinutes())
+    const seg = zeroAesquerda(data.getSeconds())
+    
+    return `${dia}/${mes}/${ano} ${hora}:${min}:${seg}`
+}
+
+const data = new Date()
+const dataBrasil = formatarData(data)
+console.log(dataBrasil)
