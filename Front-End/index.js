1a902196b9106e16415e9f2d01f0ce6586940565
adrianomts7 <161032614+adrianomts7@users.noreply.github.com>
2024-09-08 11:10:53 -0300

    Diferença entre let e var

--- Front-End/index.js
+++ Front-End/index.js
@@ -1,106 +1,39 @@
 /*
-const tela = document.querySelector("h1")
-tela.innerHTML = ""
+const verdadeira = true
 
-let data = new Date()
-let dia = data.getDay()
-let mes = data.getMonth()
-let ano = data.getFullYear()
-let hora = data.getHours()
-let minutos = data.getMinutes()
+// Let tem escopo de bloco { ... bloco }
+// Var so tem escopo de função, reescreve
 
-function adicionarZero(num){
-    return num >= 10 ? num : `0${num}`
-}
+let nome = 'Luiz' // Criando
+var nome2 = 'Luiz' // Criando
 
-function mesTexto(mes){
-    let mesTexto
-    
-    switch (mes){
-        case 0:
-            mesTexto = "Janeiro"
-            break
-        case 1:
-            mesTexto = "Fevereiro"
-            break
-        case 2:
-            mesTexto = "Março"
-            break
-        case 3:
-            mesTexto = "Abril"
-            break
-        case 4:
-            mesTexto = "Maio"
-            break
-        case 5:
-            mesTexto = "Junho"
-            break
-        case 6:
-            mesTexto = "Julho"
-            break
-        case 7:
-            mesTexto = "Agosto"
-            break
-        case 8:
-            mesTexto = "Setembro"
-            break
-        case 9:
-            mesTexto = "Outubro"
-            break
-        case 10:
-            mesTexto = "Novembro"
-            break
-        case 11:
-            mesTexto = "Dezembro"
-            break
-        default:
-            mesTexto = "Mês invalido"
+
+if (verdadeira){
+    let nome = 'Otavio' // Criando
+    var nome2 = 'Rogerio' // redeclarando
+
+    if (verdadeira){
+        var nome2 = 'Ronaldo' // Redeclarada
+        let nome = 'Outra coisa'
     }
-
-    return mesTexto
-
 }
 
-function diaTexto(dia){
-    let diaTexto
-
-    switch (dia){
-        case 0:
-            diaTexto = "Domingo"
-            break
-        case 1:
-            diaTexto = "Segunda-Feira"
-            break
-        case 2:
-            diaTexto = "Terça-Feira"
-            break
-        case 3:
-            diaTexto = "Quarta-Feira"
-            break
-        case 4:
-            diaTexto = "Quinta-feira"
-            break
-        case 5:
-            diaTexto = "Sexta-Feira"
-            break
-        case 6:
-            diaTexto = "Sábado"
-            break
-         default:
-            diaTexto = "Dia invalido"
-            break
-    }
-    return diaTexto
-}
-
-tela.innerHTML += `${diaTexto(dia)}, ${dia} de ${mesTexto(mes)} de ${ano} <br/>`
-tela.innerHTML += `${adicionarZero(hora)}:${adicionarZero(minutos)}`
+console.log(nome,nome2)
 */
-
-//Outra Maneira de se fazer forma mais facil
 /*
-let h1 = document.querySelector("h1")
-let data = new Date()
-h1.innerHTML = data.toLocaleString('Pt-BR', {dateStyle: 'full', timeStyle: 'short'})
+const verdadeira = true
+
+function falaOI(){
+    var sobrenome = 'Santos'
+
+    
+    if (verdadeira){
+        let nome = 'Luiz'
+        console.log(sobrenome)
+    }
+}
+falaOI()
 */
 
+let sobrenome = 'Santos'
+console.log(sobrenome)
