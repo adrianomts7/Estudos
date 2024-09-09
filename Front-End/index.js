1f03829c0af3c3e675a932636558d62a2e12d391
adrianomts7 <161032614+adrianomts7@users.noreply.github.com>
2024-09-09 18:50:40 -0300

    for Of

--- Front-End/index.js
+++ Front-End/index.js
@@ -1,20 +1,33 @@
-// For in -> lê indice por indice ou chaves do objeto
-//const frutas = ['Pera','Maçã','Uva']
-
-//for (let i in frutas){ 
-//  console.log(frutas[i])
-//}
-
+//const nome = 'Marta Silva'
+//const nomes = ['Marcio','Maria','Mateus']
 const pessoa = {
     nome: 'Marta',
-    sobrenome: 'Otavio',
-    idade: 30
-}
-
-//console.log(pessoa.nome) // Acessar indice de uma forma
-//const chave = 'nome'
-//console.log(pessoa[chave]) // Outra forma
+    sobrenome: 'Silva'
+};
 
 for (let chave in pessoa){
-   console.log(chave, pessoa[chave])
+    console.log(chave,pessoa[chave])
 }
+
+/* For Classico - Geralmente com iteraveis (array ou strings)
+for (let i = 0; i < nome.length; i++){
+    console.log(nome[i])
+}
+*/
+
+/* For In - Retorna o indice ou chave (string,array ou objetos)
+for (let i in nome){
+    console.log(nome[i])
+}
+*/
+
+// Quando a String for interavel utilizar o OF
+// For OF - REtorna o valor em si (iteraveis,arrays ou strings)
+for (let valor of nomes){
+    console.log(valor)
+}
+
+nomes.forEach(function(valor,indice,array){
+    console.log(valor,indice,array)
+})
+
