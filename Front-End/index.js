d4e9c7618921dfdd6633f38c354c4189ed218368
adrianomts7 <161032614+adrianomts7@users.noreply.github.com>
2024-09-09 10:34:03 -0300

    Exercicio com For e desestruturação

--- Front-End/index.js
+++ Front-End/index.js
@@ -1,5 +1,21 @@
-const frutas = ['Maçã', 'Pera','Uva','Melancia','Melão','Acerola','Cajá']
+const elementos = [
+    {tag: 'p', texto: 'Texto que você quiser'},
+    {tag: 'div', texto: 'Frase2'}, 
+    {tag: 'Footer', texto: 'Frase3'},
+    {tag: 'Section', texto: 'Frase4'}  
+]
 
-for (let i = 0; i < frutas.length; i++){
-    console.log(`Índice ${i}`, frutas[i])
+const container = document.querySelector("section");
+const div = document.createElement("div");
+
+for (let i = 0; i < elementos.length; i++){
+    let { tag, texto} = elementos[i]
+    let tagCriada = document.createElement(tag)
+    //tagCriada.innerHTML = texto //Por que não te, teag html é sim texto que será adicionado 
+    //tagCriada.innerText = texto // Pode usar assim
+    let textoCriado = document.createTextNode(texto) // Ou asssim
+    textoCriado.appendChild(textoCriado) // Para adicionar 
+    div.appendChild(tagCriada)
 }
+
+container.appendChild(div)
