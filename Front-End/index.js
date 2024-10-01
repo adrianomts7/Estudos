b2f55f9f915dc3ad3c6adeb4b247e30f6a9abb63
adrianomts7 <161032614+adrianomts7@users.noreply.github.com>
2024-10-01 18:26:38 -0300

    Diferença entre while e do while no JavaScript, no exercicio criando um gerador de número aleatorio de 1 a 50

--- Front-End/index.js
+++ Front-End/index.js
@@ -1,16 +1,21 @@
-// Seleciona os elementos que vai pegar da classe no html
-const paragrafos = document.querySelector('.paragrafos')
-// Pega o que tem nas tags,dentro da classe 
-const ps = paragrafos.querySelectorAll('p')
-
-// Pega os estilo do css da onde você escolher, abaixo pega o do body
-const estilosBody = getComputedStyle(document.body)
-// Pega o estilo do lugar escolhido (body) escolhido, como esta abaixo que pega a cor (backgroundoColor)
-const backgroundcolorBody = estilosBody.backgroundColor 
-// Exibe o rgb da cor 
-console.log(backgroundcolorBody)
-
-for(let p of ps){
-    p.style.backgroundColor = backgroundcolorBody
-    p.style.color = "white"
+function random(min,max){
+    const r = Math.random() * (max - min) + min
+    return Math.floor(r)
 }
+
+const min = 1
+const max = 50
+// let rand = random(min,max)
+let rand = 10
+
+while(rand !== 10){
+    rand = random(min, max)
+    console.log(rand)
+}
+
+console.log("#######")
+
+do{
+    rand = random(min,max)
+    console.log(rand)
+}while(rand !== 10)
