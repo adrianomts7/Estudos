d6c0fb11dbcb45d8882c6ada07a030ddbba3a492
adrianomts7 <161032614+adrianomts7@users.noreply.github.com>
2024-10-02 18:13:46 -0300

    Conhecendo Break e continue no javascript

--- Front-End/index.js
+++ Front-End/index.js
@@ -1,21 +1,17 @@
-function random(min,max){
-    const r = Math.random() * (max - min) + min
-    return Math.floor(r)
+const numeros = [1,2,3,4,5,6,7,8,9] 
+
+for(let numero of numeros){
+
+    if (numero === 2){
+        console.log('Pulei o número 2')
+        continue
+    }
+    
+    
+    if (numero === 7){
+        console.log('Parei no número 7')
+        break
+    }
+    
+    console.log(numero)
 }
-
-const min = 1
-const max = 50
-// let rand = random(min,max)
-let rand = 10
-
-while(rand !== 10){
-    rand = random(min, max)
-    console.log(rand)
-}
-
-console.log("#######")
-
-do{
-    rand = random(min,max)
-    console.log(rand)
-}while(rand !== 10)
