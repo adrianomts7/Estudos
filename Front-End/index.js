aa2c7e5ec406379359827dfb8965a323e43b504e
adrianomts7 <161032614+adrianomts7@users.noreply.github.com>
2024-10-04 19:07:13 -0300

    Ultimo exercicio de logica, esse exercicio foi feito uma função para analisar se o número e divisivel por 3 ou 5 e retorna uma mensagem ou se o número não divisivel retorna o número

--- Front-End/index.js
+++ Front-End/index.js
@@ -1,25 +1,39 @@
-// function ePaisagem(lar,alt){
-//     let paisagem = null
-//     let largura = Number(lar)
-//     let altura = Number(alt)
+// function divisivel(numero){
+//     let mensagem = null
 
-//     if(largura === 1080 && altura === 556){
-//         paisagem = true
+//     if(typeof numero !== "number"){
+//         mensagem = "Não e um número"
+//     }
+
+//     if (numero % 3 === 0 && numero % 5 === 0){
+//         mensagem = "FizBuzz"
+//     }
+//     else if (numero % 3 === 0){
+//         mensagem = "Fizz"
+//     }
+//     else if (numero % 5 === 0){
+//         mensagem = "Buuz"
 //     }
 //     else{
-//         paisagem = false
+//         mensagem = `${numero}`
 //     }
 
-//     return paisagem
+//     return mensagem
+
 // }
 
-// console.log(ePaisagem(1080,608))
-
-// function ePaisagem(x,y){
-//     return x === 1080 & y === 556 ? true : false
+// for (let i = 0; i <= 100; i++){
+//     console.log(i, divisivel(i))
 // }
 
-// console.log(ePaisagem(1000,608))
+function fiizBuzz(numero){
+    if (typeof numero !== "number") return numero
+    if (numero % 3 === 0 && numero % 5 === 0) return 'FizzBuzz'
+    if (numero % 3 === 0) return 'Fizz'
+    if (numero % 5 === 0) return 'Buzz'
+    return numero
+}
 
-const ePaisagem = (largura, altura) => largura > altura
-console.log(ePaisagem(1000,608))
+for (let i = 0; i <= 100; i++){
+    console.log(i, fiizBuzz(i))
+}
