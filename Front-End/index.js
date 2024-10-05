faed0db04b9f9e8c66831ee1498e4afa8e68114e
adrianomts7 <161032614+adrianomts7@users.noreply.github.com>
2024-10-05 08:34:51 -0300

    Aprendendo finally e as funcionalidades no tratamento de erros e exerções

--- Front-End/index.js
+++ Front-End/index.js
@@ -1,16 +1,45 @@
-function soma(x,y){
-    if (typeof x !== "number" || typeof y !== "number"){
-        throw new Error('x e y precisam ser números') // Lançando um novo erro pode usar new Error ou Reference Error
+// try{
+// // Executada quando não há erros
+//     console.log(a)
+//     console.log('Abri um arquivo')
+//     console.log('Manipulei o arquivo e gerou erro')
+//     console.log('Fechei o arquivo')
+// }
+// catch(e){
+// // Executada quando há erros
+//     console.log('Tratando o erro')
+// }
+// finally {
+// // Executa Sempre
+//     console.log('FINALLY: Eu sempre sou executado')
+// }
+
+function retornaHora(data){
+    if (data && !(data instanceof Date)){
+        throw new TypeError('Esperando instância de Date.')
     }
 
-    return x + y
-}
+    if(!data){
+        data = new Date()
+    }
 
-try{
-    console.log(soma('1',2))
-    console.log(soma(1,2))
+    return data.toLocaleTimeString('pt-BR',{
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        hour12: false
+    })
+
 }
-catch (err){
-    console.log(err) 
-    console.log('Alguma coisa mais amigavel pro usuário')
+try{
+    //const data = new Date('01-01-1970 12:58:12')
+    const hora = retornaHora(data)
+    console.log(hora)
+}
+catch (e){
+    // Tratar erro
+    console.log('Data e hora, Invalidos!')
+}
+finally {
+    console.log("Tenha um bom dia")
 }
