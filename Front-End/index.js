eba6b1f32b555f2f83ba05ee85cb572eed18b4ae
adrianomts7 <161032614+adrianomts7@users.noreply.github.com>
2024-10-05 08:56:38 -0300

    Aprendendo como utilizar o setInterval e setTimeout

--- Front-End/index.js
+++ Front-End/index.js
@@ -1,45 +1,21 @@
-// try{
-// // Executada quando não há erros
-//     console.log(a)
-//     console.log('Abri um arquivo')
-//     console.log('Manipulei o arquivo e gerou erro')
-//     console.log('Fechei o arquivo')
-// }
-// catch(e){
-// // Executada quando há erros
-//     console.log('Tratando o erro')
-// }
-// finally {
-// // Executa Sempre
-//     console.log('FINALLY: Eu sempre sou executado')
-// }
+function mostraHora(){
+    let data = new Date()
 
-function retornaHora(data){
-    if (data && !(data instanceof Date)){
-        throw new TypeError('Esperando instância de Date.')
-    }
-
-    if(!data){
-        data = new Date()
-    }
-
-    return data.toLocaleTimeString('pt-BR',{
-        hour: '2-digit',
-        minute: '2-digit',
-        second: '2-digit',
+    return data.toLocaleTimeString('pt-BR', {
         hour12: false
     })
+}
 
-}
-try{
-    //const data = new Date('01-01-1970 12:58:12')
-    const hora = retornaHora(data)
-    console.log(hora)
-}
-catch (e){
-    // Tratar erro
-    console.log('Data e hora, Invalidos!')
-}
-finally {
-    console.log("Tenha um bom dia")
-}
+const timer =  setInterval(function(){
+    console.log(mostraHora())
+},1000)
+
+setTimeout(function(){
+   clearInterval(timer)
+}, 30000)
+
+setTimeout(function(){
+    console.log("Olá Mundo")
+},5000)
+
+// console.log(mostraHora())
