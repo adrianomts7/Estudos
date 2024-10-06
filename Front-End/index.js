9b063ec2a03d83e49023c43ed0f2a8c71d91a149
adrianomts7 <161032614+adrianomts7@users.noreply.github.com>
2024-10-06 11:19:57 -0300

    As varias maneiras de declarar funções

--- Front-End/index.js
+++ Front-End/index.js
@@ -1,77 +1,28 @@
-const inputTarefa = document.querySelector('.input-tarefa')
-const btnTarefa = document.querySelector('.btn-tarefa')
-const tarefas = document.querySelector('.tarefas')
+// Declaração de função (function hoisting)
+falaOi()
 
-function criaLi(){
-    const li = document.createElement('li')
-    return li
+function falaOi(){
+    console.log("Oie")
 }
 
-inputTarefa.addEventListener('keypress',function(e){
-    if (e.keyCode === 13){
-        if (!inputTarefa.value) return
-        criaTarefa(inputTarefa.value)
-        limpaInput()
-    }
-})
-
-function limpaInput(){
-    inputTarefa.value = ''
-    inputTarefa.focus()
+// First-class objects (Objetos de primeira classe)
+// Function express
+const souUmDado = function nomeDaFuncao(){
+    console.log('Sou um dado')
 }
+souUmDado()
 
-function criaBotaoApagar(li){
-    li.innerText += ' '
-    const botaoApagar = document.createElement('button')
-    botaoApagar.innerText = 'Apagar'
-    botaoApagar.setAttribute('class', 'apagar')
-    li.appendChild(botaoApagar)
+// Arrow Function
+const funcaoArrow = () => {
+    console.log('Sou uma arrow function')
 }
+funcaoArrow()
 
-function criaTarefa(textoInput){
-    const li = criaLi()
-    li.innerText = textoInput
-    tarefas.appendChild(li)
-    limpaInput()
-    criaBotaoApagar(li)
-    salvarTarefas()
-}
-
-btnTarefa.addEventListener('click',function(){
-    if(!inputTarefa.value) return
-    criaTarefa(inputTarefa.value)
-})
-
-document.addEventListener('click',function(e){
-    const el = e.target
-    
-    if(el.classList.contains('apagar')){
-        el.parentElement.remove()
-        salvarTarefas()
-    }
-})
-
-function salvarTarefas(){
-    const liTarefas = tarefas.querySelectorAll('li')
-    const listaDeTarefas = []
-
-    for (let tarefa of liTarefas){
-        let tarefaTexto = tarefa.innerText
-        tarefaTexto = tarefaTexto.replace('Apagar', '').trim()
-        listaDeTarefas.push(tarefaTexto)
-    }
-
-    const tarefasJSON = JSON.stringify(listaDeTarefas)
-    localStorage.setItem('tarefas', tarefasJSON)
-}
-
-function adicionaTarefasSalvas(){
-    const tarefas = localStorage.getItem('tarefas')
-    const listaDeTarefas = JSON.parse(tarefas)
-
-    for(let tarefa of listaDeTarefas){
-        criaTarefa(tarefa)
+// Dentro de um objeto
+const obj = {
+    falar (){
+        console.log('Estou falando...')
     }
 }
 
-adicionaTarefasSalvas()
+obj.falar()
