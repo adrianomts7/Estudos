a969668738e704fbacea24011b0cca16c2231ccc
adrianomts7 <161032614+adrianomts7@users.noreply.github.com>
2024-10-07 10:03:42 -0300

    Aprendendo como funciona as funções imediatas

--- Front-End/index.js
+++ Front-End/index.js
@@ -1,47 +1,15 @@
-function rand(min = 1000, max= 3000){
-        const num = Math.random() * (max - min) + min
-    return Math.floor(num)
-}
+// IIFE -> Expressão de função invocada imediatamente
+(function(){
+    
+    const sobrenome = 'Mateus'
+    function criaNome(nome){
+        return nome +  ' ' + sobrenome;
+    }
 
-function f1(callback){
-    setTimeout(function(){
-        console.log("f1")
-        if (callback) callback()
-    }, rand())
-}
+    function falaNome(){
+        console.log(criaNome('Adriano'))
+    }
 
-function f2(callback){
-    setTimeout(function(){
-        console.log("f2")
-        if (callback) callback()
-    }, rand())
-}
+    falaNome()
 
-function f3(callback){
-    setTimeout(function(){
-        console.log("f3")
-        if (callback) callback()
-    }, rand())
-}
-
-// f1(function(){
-//     f2(function(){
-//         f3(function(){
-//             console.log('Olá Mundo')
-//         })
-//     })
-// })
-
-f1(f1CallBack)
-
-function f1CallBack(){
-    f2(f2CallBack)
-}
-
-function f2CallBack(){
-    f3(f3CallBack)
-}
-
-function f3CallBack(){
-    console.log("Olá Mundo!")
-}
+})()
