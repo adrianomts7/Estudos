d707c20a63047371ddac8dbcb961beaf5bd31144
adrianomts7 <161032614+adrianomts7@users.noreply.github.com>
2024-10-09 18:06:30 -0300

    factory functions aprendendo como utlizar e como ela funciona, junto com get e set

--- Front-End/index.js
+++ Front-End/index.js
@@ -1,15 +1,40 @@
-// IIFE -> Expressão de função invocada imediatamente
-(function(){
+// factory function (Função Fabrica)
+function criaPessoa(nome,sobrenome,altura,peso){
+    return{
+        nome,
+        sobrenome,
+
+        // Getter
+        get nomeCompleto(){
+            return `${this.nome} ${this.sobrenome}`
+        },
+
+        // Setter
+        set nomeCompleto(valor){
+            valor = valor.split(' ')
+            this.nome = valor.shift()
+            this.sobrenome = valor.join(' ')
+            console.log(valor)
+        },
+
+        fala(assunto = 'Falando sobre nada'){
+            return `${this.nome} esta ${assunto}.`
+        },
+
+        altura: altura,
+        peso: peso,
+
+        // Getter
+        get imc(){
+            const indice = this.peso / (this.altura ** 2)
+            return indice.toFixed(2)
+        }
     
-    const sobrenome = 'Mateus'
-    function criaNome(nome){
-        return nome +  ' ' + sobrenome;
     }
+}
 
-    function falaNome(){
-        console.log(criaNome('Adriano'))
-    }
-
-    falaNome()
-
-})()
+const p1 = criaPessoa('Adriano','Mateus',1.7,60)
+p1.nomeCompleto = "Joanice dos Santos"
+console.log(p1.nome)
+console.log(p1.sobrenome)
+console.log(p1.fala())
