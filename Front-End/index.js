fb726f7b14a691bdd43e46ccb4439486cdab9675
adrianomts7 <161032614+adrianomts7@users.noreply.github.com>
2024-10-12 15:07:41 -0300

    Fazendo exercicio da calculador usando factory functions

--- Front-End/index.js
+++ Front-End/index.js
@@ -1,40 +1,80 @@
-// factory function (Função Fabrica)
-function criaPessoa(nome,sobrenome,altura,peso){
-    return{
-        nome,
-        sobrenome,
+function criaCalculadora(){
+    return {
+        display: document.querySelector('.display'),
+        btnClear: document.querySelector('.btn-clear'),
 
-        // Getter
-        get nomeCompleto(){
-            return `${this.nome} ${this.sobrenome}`
+        inicia() {
+            this.cliqueBotoes()
+            this.pressionaEnter()
+        },
+        
+        pressionaEnter(){
+            this.display.addEventListener('keyup', e => {
+                if(e.keyCode === 13){
+                    this.realizaConta()
+                }
+            })
         },
 
-        // Setter
-        set nomeCompleto(valor){
-            valor = valor.split(' ')
-            this.nome = valor.shift()
-            this.sobrenome = valor.join(' ')
-            console.log(valor)
+        realizaConta(){
+            let conta = this.display.value
+
+            try{
+                conta = eval(conta)
+
+                if(!conta){
+                    alert('Conta invalida')
+                    return this.clearDisplay()
+                }
+
+                this.display.value = String(conta)
+            }
+            catch(e){
+                alert('Conta Invalida')
+                return this.clearDisplay()
+            }
+
+        },
+        
+        clearDisplay(){
+            this.display.value = " "
         },
 
-        fala(assunto = 'Falando sobre nada'){
-            return `${this.nome} esta ${assunto}.`
+        apagaUm(){
+            this.display.value = this.display.value.slice(0, -1) 
         },
 
-        altura: altura,
-        peso: peso,
 
-        // Getter
-        get imc(){
-            const indice = this.peso / (this.altura ** 2)
-            return indice.toFixed(2)
+        cliqueBotoes() {
+            // this -> calculadora
+            document.addEventListener('click', e => {
+                const el = e.target
+                
+                if(el.classList.contains('btn-num')){
+                    this.btnParaDisplay(el.innerText)
+                }
+                
+                if(el.classList.contains("btn-clear")){
+                    this.clearDisplay()
+                }
+
+                if (el.classList.contains('btn-del')){
+                    this.apagaUm()
+                }
+
+                if (el.classList.contains('btn-eq')){
+                    this.realizaConta()
+                }
+
+            })
+        },
+
+        btnParaDisplay(valor){
+            this.display.value += valor;
         }
-    
+
     }
 }
 
-const p1 = criaPessoa('Adriano','Mateus',1.7,60)
-p1.nomeCompleto = "Joanice dos Santos"
-console.log(p1.nome)
-console.log(p1.sobrenome)
-console.log(p1.fala())
+const calculadora = criaCalculadora()
+calculadora.inicia()
