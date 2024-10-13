596ed568412919e71bb004d3d4456a0819134c3f
adrianomts7 <161032614+adrianomts7@users.noreply.github.com>
2024-10-13 16:28:27 -0300

    Aprendendo como concatenar array com concat é spread

--- Front-End/index.js
+++ Front-End/index.js
@@ -1,15 +1,6 @@
-const nomes = ['Adriano','Lourdes','Aurora','Mateus','Maria Cecilia']
-
-// nomes.splice(indice,delete,elemento para adicionar)
-// pop -> remove o ultimo item
-// const removidos = nomes.splice(-1,1)
-
-// shift -> remove o primeiro item
-// const removidos = nomes.splice(0,1)
-
-// Push -> Adicionar no ultimo indice da lista
-// nomes.splice(nomes.length,1,'Maria')
-
-// unshift -> Adicionar no primeiro indice da lista
-nomes.splice(0,0,'Maria')
-console.log(nomes)
+const a1 = [1,2,3]
+const a2 = [4,5,6]
+// const a3 = a1.concat(a2, [7, 8, 9], 'Adriano')
+/// ... reset -> ... spread
+const a3 = [...a1,'Adriano',...a2, ...[7,8,9]]
+console.log(a3)
