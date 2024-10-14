282f76cd4945e447fb58cff90f837fd62b75706d
adrianomts7 <161032614+adrianomts7@users.noreply.github.com>
2024-10-14 18:23:24 -0300

    Aprendendo como funciona o reduce

--- Front-End/index.js
+++ Front-End/index.js
@@ -1,14 +1,14 @@
-// Dobre os números
-const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22,27]
-const numerosEmDobro = numeros.map(function(valor) { return valor * 2 })
+// Some todos os números (reduce)
 
-// console.log(numerosEmDobro)
+const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27]
+const total = numeros.reduce(function(acumulador, valor) {
+    return acumulador += valor
+})
 
-// Para cada Elemento
-// Retorne apenas uma string com o nome da pessoa
-// Remova apenas a chave "nome" do objeto
-// Adicione uma chave id em cada objeto
+// console.log(total)
 
+
+// Retorne a pessoa mais velha
 const pessoas = [
     { nome: 'Maria', idade: 20},
     { nome: 'Nego', idade: 30},
@@ -18,22 +18,8 @@ const pessoas = [
     { nome: 'Marilene', idade: 25}
 ]
 
-const nomes = pessoas.map(valor => valor.nome)
-// console.log(nomes)
-
-const idades = pessoas.map(valor => ({idade: valor.idade}))
-// console.log(idades)
-
-// const idades = pessoas.map(valor => {
-//     delete valor.nome
-//     return {idade: valor.idade}
-// })
-// console.log(idades)
-
-const adicionandoID = pessoas.map((valor, indice) => {
-    const newObj = {... valor}
-    newObj.id = (indice + 1)
-    return newObj
+const maisVelha = pessoas.reduce(function(acumulador, valor){
+    if(acumulador.idade > valor.idade) return acumulador
+    return valor
 })
-console.log(adicionandoID)
-console.log(pessoas)
+console.log(maisVelha)
