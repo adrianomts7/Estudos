157d26dd44938af7e92469a2e4f66101bd7cb465
adrianomts7 <161032614+adrianomts7@users.noreply.github.com>
2024-10-15 16:44:48 -0300

    Aprendendo a usar o forEach

--- Front-End/index.js
+++ Front-End/index.js
@@ -1,8 +1,4 @@
-// Retorne a soma do dobro de todos os pares
-// -> Filtrar pares
-// -> dobrar os valores
-// -> Reduzir (somar tudo)
-
-const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27]
-const numerosPares = numeros.filter(valor => valor % 2 === 0).map(valor => valor * 2).reduce((acumulador,valor) => acumulador += valor)
-console.log(numerosPares)
+const a1 = [10, 20, 30, 40, 50, 60, 70, 80, 90]
+let total = 0
+a1.forEach(valor => { total += valor })
+console.log(total)
