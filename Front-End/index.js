8a2b7f5fdbd98b02662a993c2b4dc02aace56dd3
adrianomts7 <161032614+adrianomts7@users.noreply.github.com>
2024-10-15 16:33:16 -0300

    Utilizando as três funções juntas, filter,map e reduce

--- Front-End/index.js
+++ Front-End/index.js
@@ -1,25 +1,8 @@
-// Some todos os números (reduce)
+// Retorne a soma do dobro de todos os pares
+// -> Filtrar pares
+// -> dobrar os valores
+// -> Reduzir (somar tudo)
 
 const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27]
-const total = numeros.reduce(function(acumulador, valor) {
-    return acumulador += valor
-})
-
-// console.log(total)
-
-
-// Retorne a pessoa mais velha
-const pessoas = [
-    { nome: 'Maria', idade: 20},
-    { nome: 'Nego', idade: 30},
-    { nome: "Ana", idade: 50},
-    { nome: 'Joanice', idade: 35},
-    { nome: 'Nicacio', idade: 80},
-    { nome: 'Marilene', idade: 25}
-]
-
-const maisVelha = pessoas.reduce(function(acumulador, valor){
-    if(acumulador.idade > valor.idade) return acumulador
-    return valor
-})
-console.log(maisVelha)
+const numerosPares = numeros.filter(valor => valor % 2 === 0).map(valor => valor * 2).reduce((acumulador,valor) => acumulador += valor)
+console.log(numerosPares)
