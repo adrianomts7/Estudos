46e34240bc3aef0d21da32d1c3eebe3650b3f731
adrianomts7 <161032614+adrianomts7@users.noreply.github.com>
2024-10-15 18:14:18 -0300

    Aprendendo como funciona os metodos defineProperty e defineProperties

--- Front-End/index.js
+++ Front-End/index.js
@@ -1,61 +1,30 @@
-// Literal
-const pessoa = {
-    nome: 'Adriano',
-    sobrenome: 'Mateus',
-}
+// defineProperty -> define somente uma
+// defineProperties -> define varias de uma vez
 
-const chave = 'nome'
-// console.log(pessoa[chave])
-// console.log(pessoa.sobrenome)
+function Produto(nome, preco, estoque){
+    Object.defineProperty(this, 'estoque', {
+        enumerable: true, // Mostra a chave 
+        value: estoque, // Valor
+        writable: false, // Posso Alterar o valor do estoque
+        configurable: false // configuravel
+    })
 
-// Construtor
-const pessoa1 = new Object()
-pessoa1.nome = "Adriano"
-pessoa1.sobrenome = 'Mateus'
-pessoa1.idade = 19
-
-// Metodo
-pessoa1.falarNome = function() {
-    return (`Olá ${this.nome}`)
-}
-
-// Metodo
-pessoa1.getDataNascimento = function() {
-    const dataAtual = new Date()
-    return dataAtual.getFullYear() - this.idade
-}
-
-// delete pessoa1.nome
-// console.log(pessoa1)
-// pessoa1.falarNome()
-// console.log(pessoa1.getDataNascimento())
-
-// Factory functions
-function criaPessoa(nome, sobrenome){
-    return {
-        nome,
-        sobrenome,
-        get nomeCompleto() {
-            return `${this.nome} ${this.sobrenome}`
+    Object.defineProperties(this, {
+        nome: {
+            enumerable: true,
+            value: nome,
+            writable: true,
+            configurable: true
+        },
+        preco: {
+            enumerable: true,
+            value: preco,
+            writable: true,
+            configurable: true
         }
-    }
+    })
+
 }
 
-const p1 = criaPessoa('Adriano', 'Mateus')
-// console.log(p1)
-
-// Constructor function
-function Pessoa(nome, sobrenome){
-    this.nome = nome
-    this.sobrenome = sobrenome
-}
-
-const p2 = new Pessoa('Adriano', 'Mateus')
-// console.log(p2)
-
-const p3 = {}
-p3.nome = 'Adriano'
-p3['sobrenome'] = 'Mateus'
-
-Object.freeze(p3)
-// console.log(p3)
+const p1 = new Produto('Camiseta', 20, 7)
+console.log(p1)
