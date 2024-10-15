199f4a3795ef8eeebcfd406d7f6e3dedcf5ea069
adrianomts7 <161032614+adrianomts7@users.noreply.github.com>
2024-10-15 17:42:27 -0300

    Revisando Objetos

--- Front-End/index.js
+++ Front-End/index.js
@@ -1,4 +1,61 @@
-const a1 = [10, 20, 30, 40, 50, 60, 70, 80, 90]
-let total = 0
-a1.forEach(valor => { total += valor })
-console.log(total)
+// Literal
+const pessoa = {
+    nome: 'Adriano',
+    sobrenome: 'Mateus',
+}
+
+const chave = 'nome'
+// console.log(pessoa[chave])
+// console.log(pessoa.sobrenome)
+
+// Construtor
+const pessoa1 = new Object()
+pessoa1.nome = "Adriano"
+pessoa1.sobrenome = 'Mateus'
+pessoa1.idade = 19
+
+// Metodo
+pessoa1.falarNome = function() {
+    return (`Olá ${this.nome}`)
+}
+
+// Metodo
+pessoa1.getDataNascimento = function() {
+    const dataAtual = new Date()
+    return dataAtual.getFullYear() - this.idade
+}
+
+// delete pessoa1.nome
+// console.log(pessoa1)
+// pessoa1.falarNome()
+// console.log(pessoa1.getDataNascimento())
+
+// Factory functions
+function criaPessoa(nome, sobrenome){
+    return {
+        nome,
+        sobrenome,
+        get nomeCompleto() {
+            return `${this.nome} ${this.sobrenome}`
+        }
+    }
+}
+
+const p1 = criaPessoa('Adriano', 'Mateus')
+// console.log(p1)
+
+// Constructor function
+function Pessoa(nome, sobrenome){
+    this.nome = nome
+    this.sobrenome = sobrenome
+}
+
+const p2 = new Pessoa('Adriano', 'Mateus')
+// console.log(p2)
+
+const p3 = {}
+p3.nome = 'Adriano'
+p3['sobrenome'] = 'Mateus'
+
+Object.freeze(p3)
+// console.log(p3)
