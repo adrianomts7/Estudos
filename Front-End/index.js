23ad3630f824d5271128276abc5e13b6ca036486
adrianomts7 <161032614+adrianomts7@users.noreply.github.com>
2024-10-17 18:26:45 -0300

    Aprendendo outros metedos para usar em objetos

--- Front-End/index.js
+++ Front-End/index.js
@@ -1,41 +1,39 @@
-// defineProperty -> Getter e Setters
-function Produto(nome, preco, estoque){
-    this.nome = nome
-    this.preco = preco
-     
-    let estoquePrivado = estoque
-    Object.defineProperty(this, 'estoque', {
-        enumerable: true, // Mostra a chave 
-        configurable: false, // configuravel
-        get: function (){
-            return estoquePrivado
-        },
-        set: function (valor){
-            if(typeof valor !== Number){
-               throw new TypeError("Digite somente números")
-            }
+/*
+// Estamos vendo
+Object.values
+Object.entries
+Objetc.assign(des, any)
+Objetc.getOwnPropertyDescriptor(o, 'prop')
+... (spread)
 
-            estoquePrivado = valor
-        }
-    })
+// Já vimos
+Object.keys (retorna chaves)
+Objetc.freeze (congela o objeto)
+Objetc.defineProperties (define varias propriedades)
+Objetc.defineProperty (define yma propriedade)
+*/
 
+const produto = { nome: 'Produto', preco: 1.8}
+// Object.freeze(produto)
+
+const caneca = Object.assign({}, produto, {material: 'porcelana'})
+
+caneca.nome = 'Caneca'
+caneca.preco = 2.5
+
+// console.log(produto)
+// console.log(caneca)
+// console.log(Object.keys(produto))
+Object.defineProperty(produto, 'nome', {
+    configurable: false,
+    writable: false,
+    value: 'Qualquer outra coisa'
+})
+
+// console.log(Object.getOwnPropertyDescriptor(produto, 'nome'))
+// console.log(produto)
+// console.log(Object.entries(produto))
+
+for(let [chave,valor] of Object.entries(caneca)){
+    console.log(chave, valor)
 }
-
-function criaProduto(nome) {
-    return {
-        get nome (){
-            return nome
-        },
-        set nome (valor){
-            valor = valor.replace('Coisa', " ")
-            nome = valor
-        }
-    }
-}
-
-const p1 = new Produto('Camiseta', 20, 7)
-// console.log(p1)
-
-const p2 = criaProduto("Camiseta")
-p2.nome = 'Qualquer Coisa'
-console.log(p2.nome)
