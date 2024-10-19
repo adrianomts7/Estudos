6b7f4da8010f5e0acee40db83aa62e8ee882c54b
adrianomts7 <161032614+adrianomts7@users.noreply.github.com>
2024-10-19 14:48:11 -0300

    Aprendendo como manipular prototypes

--- Front-End/index.js
+++ Front-End/index.js
@@ -1,14 +1,64 @@
-// Construtora -> molde (classe)
-function Pessoa(nome, sobrenome){
+// new object -> Object.prototype
+const objA = {
+    chaveA: 'A'
+    // __proto__: Object.prototype
+}
+
+const objB = {
+    chaveB: 'B',
+    // __proto__: objA
+}
+
+const objC = new Object()
+objC.chaveC = 'C'
+
+Object.setPrototypeOf(objB,objA)
+// console.log(objB.chaveA)
+Object.setPrototypeOf(objC,objB)
+// console.log(objC.chaveB)
+
+
+
+function Produto(nome, preco){
     this.nome = nome
-    this.sobrenome = sobrenome
+    this.preco = preco
 }
 
-Pessoa.prototype.nomeCompleto = function (){
-    return this.nome + ' ' + this.sobrenome
+Produto.prototype.desconto = function(valor){
+    this.preco = this.preco - (this.preco * (valor / 100))
 }
 
-// instância
-const pessoa1 = new Pessoa('Adriano', 'M.') // <- Pessoa = Função construtora
+Produto.prototype.aumento = function(valor){
+    this.preco = this.preco + (this.preco * (valor / 100))
+}
 
-console.dir(pessoa1)
+const p1 = new Produto('Camiseta', 50)
+// p1.desconto(100)
+// p1.aumento(100)
+
+const p2 = {
+    nome: 'Caneca',
+    preco: 15
+}
+
+Object.setPrototypeOf(p2, Produto.prototype)
+
+// console.log(p1)
+// p2.aumento(10)
+// console.log(p2)
+
+const p3 = Object.create(Produto.prototype, {
+    tamanho: {
+        writable: true,
+        configurable: true,
+        enumerable: true,
+        value: 40
+    },
+    largura: {
+        writable: true,
+        configurable: false,
+        enumerable: true,
+        value: 10
+    }
+})
+console.log(p3)
