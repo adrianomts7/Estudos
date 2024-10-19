17c5808d953ddc7b8bada5c64413a2725f62c125
adrianomts7 <161032614+adrianomts7@users.noreply.github.com>
2024-10-19 14:01:23 -0300

    Aprendenco como usar e como funciona o prototype

--- Front-End/index.js
+++ Front-End/index.js
@@ -1,39 +1,14 @@
-/*
-// Estamos vendo
-Object.values
-Object.entries
-Objetc.assign(des, any)
-Objetc.getOwnPropertyDescriptor(o, 'prop')
-... (spread)
-
-// Já vimos
-Object.keys (retorna chaves)
-Objetc.freeze (congela o objeto)
-Objetc.defineProperties (define varias propriedades)
-Objetc.defineProperty (define yma propriedade)
-*/
-
-const produto = { nome: 'Produto', preco: 1.8}
-// Object.freeze(produto)
-
-const caneca = Object.assign({}, produto, {material: 'porcelana'})
-
-caneca.nome = 'Caneca'
-caneca.preco = 2.5
-
-// console.log(produto)
-// console.log(caneca)
-// console.log(Object.keys(produto))
-Object.defineProperty(produto, 'nome', {
-    configurable: false,
-    writable: false,
-    value: 'Qualquer outra coisa'
-})
-
-// console.log(Object.getOwnPropertyDescriptor(produto, 'nome'))
-// console.log(produto)
-// console.log(Object.entries(produto))
-
-for(let [chave,valor] of Object.entries(caneca)){
-    console.log(chave, valor)
+// Construtora -> molde (classe)
+function Pessoa(nome, sobrenome){
+    this.nome = nome
+    this.sobrenome = sobrenome
 }
+
+Pessoa.prototype.nomeCompleto = function (){
+    return this.nome + ' ' + this.sobrenome
+}
+
+// instância
+const pessoa1 = new Pessoa('Adriano', 'M.') // <- Pessoa = Função construtora
+
+console.dir(pessoa1)
