ab2cc092200cc3ff29f632ad03b63abf00dc5260
adrianomts7 <161032614+adrianomts7@users.noreply.github.com>
2024-10-21 14:39:38 -0300

    Exercicio Validador de Cpf, feito com recursos aprendidos ate aqui no curso

--- Front-End/index.js
+++ Front-End/index.js
@@ -1,60 +1,51 @@
-function Produto(nome, preco){
-    this.nome = nome
-    this.preco = preco
-}
-
-// Soma
-Produto.prototype.aumento = function (quantia){
-    this.preco += quantia
-}
-
-Produto.prototype.desconto = function(quantia){
-    this.preco -= quantia
-}
-
-function Camiseta(nome, preco, cor){
-    Produto.call(this, nome, preco)
-    this.cor = cor
-}
-
-// Object.setPrototypeOf(Camiseta.prototype, Produto.prototype)
-Camiseta.prototype = Object.create(Produto.prototype) 
-Camiseta.prototype.constructor = Camiseta
-
-// Porcentagem
-Camiseta.prototype.aumento = function (valor){
-    this.preco = this.preco + (this.preco * (valor / 100))
-}
-
-function Caneca(nome, preco, material, estoque){
-    Produto.call(this, nome, preco)
-    this.material = material
-    
-    Object.defineProperty(this, 'estoque', {
+function ValidaCPF(cpfEnviado){
+    Object.defineProperty(this,'cpfLimpo', {
         enumerable: true,
-        configurable: false,
-        get: function(){
-            return estoque
-        },
-        set: function(valor){
-            if(typeof valor !== 'number') return
-            estoque = valor
+        get: function() {
+            return cpfEnviado.replace(/\D+/g, '')
         }
     })
-
 }
 
-Caneca.prototype = Object.create(Produto.prototype)
-Caneca.prototype.constructor = Caneca
+ValidaCPF.prototype.valido = function() {
+    if(typeof this.cpfLimpo === 'undefined') return false
+    if(this.cpfLimpo.length !== 11) return false
+    if(this.isSequencial()) return false
+    
+    const cpfParcial = this.cpfLimpo.slice(0, -2)
+    const primeiroDigito = this.criaDigito(cpfParcial)
+    const segundoDigito = this.criaDigito(cpfParcial + primeiroDigito)
+    
+    const novoCpf = cpfParcial + primeiroDigito + segundoDigito
 
-const produto = new Produto('Café', 10)
-const camiseta = new Camiseta('Regata', 7.5, 'Preta')
-const caneca = new Caneca('Caneca', 13, 'Plastico', 5)
+    return novoCpf === this.cpfLimpo
+}
 
-// caneca.estoque = 100
+ValidaCPF.prototype.criaDigito = function(cpfParcial) {
+    const cpfArray = Array.from(cpfParcial)
+    let regressivo = cpfArray.length + 1
 
-// camiseta.aumento(10)
-console.log(produto)
-console.log(camiseta)
-console.log(caneca)
-console.log(caneca.estoque)
+    const total = cpfArray.reduce((ac, val) => {
+        ac += (regressivo * Number(val))
+        regressivo --
+        return ac
+    }, 0)
+
+    const digito = 11 - (total % 11)
+
+    return digito > 9 ? '0' : String(digito)
+}
+
+ValidaCPF.prototype.isSequencial = function() {
+    const sequencia = this.cpfLimpo[0].repeat(this.cpfLimpo.length)
+    return sequencia === this.cpfLimpo
+}
+
+const cpf = new ValidaCPF('705.484.450-52')
+
+if(cpf.valido()){
+    console.log('Cpf Valido')
+}
+else {
+    console.log('Cpf Invalido')
+}
