ed0d1edbed617db8d46a2a265675a3cf009f77d4
adrianomts7 <161032614+adrianomts7@users.noreply.github.com>
2024-10-24 09:14:05 -0300

    Aprendendo como utilizar factory functions junto com prototypes

--- Front-End/index.js
+++ Front-End/index.js
@@ -1,61 +1,33 @@
-// Superclass
-function Conta(agencia, conta, saldo){
-    this.agencia = agencia
-    this.conta = conta
-    this.saldo = saldo
+const falar = {
+    falar(){
+        console.log(`${this.nome} está falando`)
+        },
 }
 
-Conta.prototype.sacar = function(valor) {
-    if(this.saldo < valor){
-        console.log(`Saldo Insuficiente: R$ ${this.saldo}`) 
-        return
+const comer = {
+    comer(){
+        console.log(`${this.nome} está comendo`)
+    },
+}
+
+const beber = {
+    beber(){
+        console.log(`${this.nome} está bebendo`)
     }
-
-    this.saldo -= valor
-    this.verSaldo()
 }
 
-Conta.prototype.depositar = function(valor) {
-    this.saldo += valor
-    this.verSaldo()
-}
+// const pessoaPrototype = { ... falar, ... comer, ... beber}
+const pessoaPrototype = Object.assign({}, falar, comer, beber)
 
-Conta.prototype.verSaldo = function() {
-    console.log(`Agencia: ${this.agencia} - Conta: ${this.conta} - Saldo: R$ ${this.saldo.toFixed(2)}`)
-}
-
-function ContaCorrente(agencia, conta, saldo, limite) {
-    Conta.call(this, agencia, conta, saldo)
-    this.limite = limite
-}
-
-ContaCorrente.prototype = Object.create(Conta.prototype)
-
-ContaCorrente.prototype.sacar = function(valor){
-    if(valor > (this.saldo + this.limite)){
-        console.log(`Saldo Insuficiente: R$ ${this.saldo}`)
-        return
-    }
-
-    this.saldo -= valor
-    this.verSaldo()
-}
-
-const cc = new ContaCorrente(11, 22, 0, 100)
-cc.depositar(10)
-cc.sacar(110)
-cc.sacar(111)
-
-function ContaPoupanca(agencia, conta, saldo){
-    Conta.call(this, agencia, conta, saldo)
+function criaPessoa(nome, sobrenome){
     
+    return Object.create(pessoaPrototype, {
+        nome: { value: nome },
+        sobrenome: { value: sobrenome }
+    })
 }
 
-ContaPoupanca.prototype = Object.create(Conta.prototype)
-ContaPoupanca.prototype.constructor = ContaPoupanca
-
-console.log()
-const cp = new ContaPoupanca(12, 33, 0)
-cp.depositar(10)
-cp.sacar(110)
-cp.sacar(1)
+const p1 = criaPessoa("Adriano", "Mateus")
+const p2 = criaPessoa("Maria", "Lourdes")
+console.log(p2.falar())
+console.log(p1.beber())
