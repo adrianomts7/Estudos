3b857d673cd93cacfb972196e2af40062ec5ccbb
adrianomts7 <161032614+adrianomts7@users.noreply.github.com>
2024-10-25 18:39:03 -0300

    Arquivos criados

--- Back-End/Python/gerenciamentoanimais/models/servico.py
+++
@@ -1,9 +0,0 @@
-class Servico:
-    def __init__(self,nomeDoServico: str,preco: float) -> None:
-        self.nomeDoServico = nomeDoServico
-        self.preco = preco
-        
-    def exibirResultado(self) -> str:
-        return ( f"Nome do Serviço: {self.nomeDoServico}"
-            f"Preço: R$ {self.preco}"
-        )
