755a39927cf4d921602b3962abf701d59514f207
adrianomts7 <161032614+adrianomts7@users.noreply.github.com>
2024-11-04 17:22:59 -0300

    Aprendendo como validar formulario

--- Front-End/index.js
+++ Front-End/index.js
@@ -1,4 +1,115 @@
-/// 705.484.450-52 - 070.987.720-03
+class ValidaFormulario{
+    constructor(){
+        this.formulario = document.querySelector('.formulario')
+
+        this.eventos()
+    }
+    
+    eventos(){
+        this.formulario.addEventListener('submit', e => {
+            this.handleSubmit(e)
+        })
+    }
+
+    handleSubmit(e){
+        e.preventDefault()
+        const camposValidos = this.camposSaoValidos()
+        const senhasValidas = this.senhasSaoValidas()
+
+        if(camposValidos && senhasValidas){
+            alert('Formulario enviado')
+            this.formulario.submit()
+        }
+    }
+
+    senhasSaoValidas(){
+        let valid = true
+
+        const senha = this.formulario.querySelector('#senha')
+        const repetirSenha = this.formulario.querySelector('#senha-repetida')
+
+        if(senha.value !== repetirSenha.value){
+            valid = false
+            this.criaErro(senha, 'Campos senha e repetir senha precisam ser iguais')
+            this.criaErro(repetirSenha, 'Campos senha e repetir senha precisam ser iguais')
+        }
+
+        if(senha.value.length < 6 || senha.value.length > 12){
+            valid = false
+            this.criaErro(senha, 'Senha precisa estar entre 6 e 12 caracteres.')
+        }
+
+        return valid
+    }
+
+    camposSaoValidos(){
+        let valid = true
+
+        for(let erro of this.formulario.querySelectorAll('.erro')){
+            erro.remove()
+        }
+
+        for(let campo of this.formulario.querySelectorAll('.validar')){
+            let label = campo.previousElementSibling.innerText
+            
+            if(!campo.value){
+                this.criaErro(campo, `Campo ${label} não pode estar em branco`)
+                valid = false
+            }
+
+            if(campo.classList.contains('cpf')){
+                if(!this.validaCpf(campo)) valid = false
+            }
+
+            if(campo.classList.contains('usuario')){
+                if(!this.validaUsuario(campo)) valid = false
+            }
+
+        }
+
+        return valid
+    }
+
+    validaUsuario(campo){
+        const usuario = campo.value
+        let valid = true
+
+        if(usuario.length < 3 || usuario.length > 12){
+            this.criaErro(campo, 'Usuário precisa ter entre 3 e 12 caracteres. ')
+            valid = false
+        }
+
+        if(!usuario.match(/^[a-zA-Z0-9]+$/g)){
+            this.criaErro(campo, 'Nome de usário precisa conter apenas letras ou números')
+        }
+
+        return true
+    }
+
+    validaCpf(campo){
+        const cpf = new ValidaCPF(campo.value)
+
+        if(!cpf.valida()){
+            this.criaErro(campo, 'CPF Inválido')
+            return false
+        }
+
+        return true
+    }
+
+    criaErro(campo, msg){
+        const div = document.createElement('div')
+        div.innerHTML = msg
+        div.classList.add('erro')
+        campo.insertAdjacentElement('afterend', div)
+    }
+
+}
+
+const valida = new ValidaFormulario()
+
+
+// Classe para validar cpf
 class ValidaCPF{
     constructor(cpfEnviado){
         Object.defineProperty(this, 'cpfLimpo',{
@@ -15,8 +126,8 @@ class ValidaCPF{
 
     geraNovoCpf(){
         const cpfSemDigitos = this.cpfLimpo.slice(0, -2)
-        const primeiroDigito = ValidaCPF.geraDigito(cpfSemDigitos)
-        const segundoDigito = ValidaCPF.geraDigito(cpfSemDigitos + primeiroDigito)
+        const primeiroDigito = this.geraDigito(cpfSemDigitos)
+        const segundoDigito = this.geraDigito(cpfSemDigitos + primeiroDigito)
 
         this.novoCpf = cpfSemDigitos + primeiroDigito + segundoDigito
     }
@@ -47,14 +158,4 @@ class ValidaCPF{
         return this.novoCpf === this.cpfLimpo
     }
 
-}
-
-let validacpf = new ValidaCPF('070.987.720-03')
-validacpf = new ValidaCPF('999.999.999-99')
-
-if(validacpf.valida()){
-    console.log("CPF válido")
-}
-else{
-    console.log('CPF invalido')
 }
