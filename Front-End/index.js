8becc779a55c68663f02d9940d96bd742cd652c0
adrianomts7 <161032614+adrianomts7@users.noreply.github.com>
2024-11-28 16:50:24 -0300

    Aprendendo como funciona Promises'

--- Front-End/index.js
+++ Front-End/index.js
@@ -1,161 +1,32 @@
-class ValidaFormulario{
-    constructor(){
-        this.formulario = document.querySelector('.formulario')
-
-        this.eventos()
-    }
-    
-    eventos(){
-        this.formulario.addEventListener('submit', e => {
-            this.handleSubmit(e)
-        })
-    }
-
-    handleSubmit(e){
-        e.preventDefault()
-        const camposValidos = this.camposSaoValidos()
-        const senhasValidas = this.senhasSaoValidas()
-
-        if(camposValidos && senhasValidas){
-            alert('Formulario enviado')
-            this.formulario.submit()
-        }
-    }
-
-    senhasSaoValidas(){
-        let valid = true
-
-        const senha = this.formulario.querySelector('#senha')
-        const repetirSenha = this.formulario.querySelector('#senha-repetida')
-
-        if(senha.value !== repetirSenha.value){
-            valid = false
-            this.criaErro(senha, 'Campos senha e repetir senha precisam ser iguais')
-            this.criaErro(repetirSenha, 'Campos senha e repetir senha precisam ser iguais')
-        }
-
-        if(senha.value.length < 6 || senha.value.length > 12){
-            valid = false
-            this.criaErro(senha, 'Senha precisa estar entre 6 e 12 caracteres.')
-        }
-
-        return valid
-    }
-
-    camposSaoValidos(){
-        let valid = true
-
-        for(let erro of this.formulario.querySelectorAll('.erro')){
-            erro.remove()
-        }
-
-        for(let campo of this.formulario.querySelectorAll('.validar')){
-            let label = campo.previousElementSibling.innerText
-            
-            if(!campo.value){
-                this.criaErro(campo, `Campo ${label} não pode estar em branco`)
-                valid = false
-            }
-
-            if(campo.classList.contains('cpf')){
-                if(!this.validaCpf(campo)) valid = false
-            }
-
-            if(campo.classList.contains('usuario')){
-                if(!this.validaUsuario(campo)) valid = false
-            }
-
-        }
-
-        return valid
-    }
-
-    validaUsuario(campo){
-        const usuario = campo.value
-        let valid = true
-
-        if(usuario.length < 3 || usuario.length > 12){
-            this.criaErro(campo, 'Usuário precisa ter entre 3 e 12 caracteres. ')
-            valid = false
-        }
-
-        if(!usuario.match(/^[a-zA-Z0-9]+$/g)){
-            this.criaErro(campo, 'Nome de usário precisa conter apenas letras ou números')
-        }
-
-        return true
-    }
-
-    validaCpf(campo){
-        const cpf = new ValidaCPF(campo.value)
-
-        if(!cpf.valida()){
-            this.criaErro(campo, 'CPF Inválido')
-            return false
-        }
-
-        return true
-    }
-
-    criaErro(campo, msg){
-        const div = document.createElement('div')
-        div.innerHTML = msg
-        div.classList.add('erro')
-        campo.insertAdjacentElement('afterend', div)
-    }
-
+function rand (min, max){
+    min *= 1000
+    max *= 1000
+    return Math.floor(Math.random() * (max - min) + min)
 }
 
-const valida = new ValidaFormulario()
+function esperaAi (msg, tempo){
+    return new Promise((resolve, reject) => {
+        if(typeof msg !== 'string') reject(new Error('ERRO'))
 
-
-// Classe para validar cpf
-class ValidaCPF{
-    constructor(cpfEnviado){
-        Object.defineProperty(this, 'cpfLimpo',{
-            writable: false,
-            enumerable: true,
-            configurable: false,
-            value: cpfEnviado.replace(/\D+/g, '')
-        })
-    }
-
-    eSequencia(){
-        return this.cpfLimpo.charAt(0).repeat(11) === this.cpfLimpo
-    }
-
-    geraNovoCpf(){
-        const cpfSemDigitos = this.cpfLimpo.slice(0, -2)
-        const primeiroDigito = this.geraDigito(cpfSemDigitos)
-        const segundoDigito = this.geraDigito(cpfSemDigitos + primeiroDigito)
-
-        this.novoCpf = cpfSemDigitos + primeiroDigito + segundoDigito
-    }
-
-    geraDigito(cpfSemDigitos){
-        let total = 0
-        let reverso = cpfSemDigitos.length + 1
-
-        for(let numeroString of cpfSemDigitos){
-            total += reverso * Number(numeroString)
-            reverso --
-            
-        }
-        
-        const digito = 11 - (total % 11)
-        return digito <= 9 ? String(digito) : '0'
-    }
-    
-    
-    valida(){
-        if(!this.cpfLimpo) return false
-        if(typeof this.cpfLimpo !== 'string') return false
-        if(this.cpfLimpo.length !== 11) return false
-        if(this.eSequencia()) return false
-        
-        this.geraNovoCpf()
-
-        return this.novoCpf === this.cpfLimpo
-    }
-
+        setTimeout(() => {
+            resolve(msg)
+        }, tempo)
+    })
 }
+
+esperaAi('Conexão com o BD', rand(1,3)).then(resposta => {
+    console.log(resposta)
+    return esperaAi('Buscando dados da base', rand(1,3))
+}).then(resposta => {
+    console.log(resposta)
+    return esperaAi(2222, rand(1, 3))
+}).then(resposta => {
+    console.log(resposta)
+}).then(() => {
+    console.log('Exibe os dados na tela.')
+})
+.catch(e => {
+    console.log('ERRO', e)
+})
+
+console.log('Isso aqui será exibido antes de qualquer promisse')
