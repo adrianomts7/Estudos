6fe083b5cd71a1ad99dc56d5cdfeb818506ee8dd
adrianomts7 <161032614+adrianomts7@users.noreply.github.com>
2024-12-19 09:48:39 -0300

    Mudando algumas coisas

--- Front-End/src/index.js
+++ Front-End/src/index.js
@@ -1,9 +1,1 @@
-import GeraCPF from './modules/GeraCPF'
-
 import './assets/css/style.css'
-
-(function(){
-    const gera = new GeraCPF()
-    const cpfGerado = document.querySelector('.resultado')
-    cpfGerado.innerHTML = gera.geraNovoCpf()
-})()
