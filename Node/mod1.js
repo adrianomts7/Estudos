b7e6306b28c370497ed808e7f80d35e7908af7fe
adrianomts7 <161032614+adrianomts7@users.noreply.github.com>
2024-12-26 11:29:41 -0300

    Aprendendo como utilizar como modules com node.js

--- Node/mod1.js
+++ Node/mod1.js
@@ -1,19 +1,31 @@
+// const nome = 'Adriano'
+// const sobrenome = 'Mateus'
+
+// const falaNome = () => nome + ' ' + sobrenome
+
+// // module.exports.nome = nome
+// // module.exports.sobrenome = sobrenome
+// // module.exports.falaNome = falaNome
+
+// exports.nome = nome
+// exports.sobrenome = sobrenome
+// exports.falaNome = falaNome
+
+// // O this e relacionado com o exports ou modules "Exports é a Abreviação do module.exports"
+// this.qualquerCoisa = 'O que eu quiser exportar'
+
+// // console.log(exports) 
+
+class Pessoa{
+    constructor(nome){
+        this.nome = nome
+    }
+}
+
 const nome = 'Adriano'
 const sobrenome = 'Mateus'
 
-const falaNome = () => {
-    console.log(nome, sobrenome)
+// Para exportar mais de um
+module.exports = {
+    nome, sobrenome, Pessoa
 }
-
-// module.exports.nome = nome
-// module.exports.sobrenome = sobrenome
-// module.exports.falaNome = falaNome
-
-exports.NOME = nome
-exports.sobrenome = sobrenome
-exports.falaNome = falaNome
-
-// O this e relacionado com o exports ou modules "Exports é a Abreviação do module.exports"
-this.qualquerCoisa = 'O que eu quiser exportar'
-
-console.log(exports)
