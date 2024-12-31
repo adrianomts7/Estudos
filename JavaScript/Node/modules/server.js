cfea1bf3cde04a2c4871b2ff442df848bf7e42e3
adrianomts7 <161032614+adrianomts7@users.noreply.github.com>
2024-12-31 15:26:48 -0300

    Aprendendo outras funcionalidades do express

--- JavaScript/Node/modules/server.js
+++ JavaScript/Node/modules/server.js
@@ -1,28 +1,29 @@
 const express = require('express')
 const app = express()
 
-//          CRIAR   LER  ATUALIZAR  APAGAR
-// CRUD -> CREATE, READ,  UPDATE,   DELETE
-//          POST    GET     PUT     DELETE  
-
-// http://meusite.com/ <- GET -> Entregue a página
-// http://meusite.com/sobre <- GET -> Entregue a página / sobre
+app.use(express.urlencoded({ extended: true}))
 
 app.get('/', (req, res) => {
     res.send(`
         <form action="/" method="POST">
         Nome do Cliente: <input type"text" name="nome">
-        <button>Olá Mundo</button>
+        <br>
+        Sobrenome do Cliente <input type="text" name="sobrenome">
+        <button>Concluir cadastro</button>
         </form>
     `)
 })
 
-app.post('/', (req, res) => {
-    res.send('Recebi o formulario')
+app.get('/teste/:idUsuario?/:parametro?', (req,res) => {
+    console.log(req.params) // Parâmetro que você digita na url, a "?" é opcional
+    console.log(req.query) // Parâmetro que pode poegar da url sem ser os obrigarios "params"
+    // res.send(req.params)
+    res.send(req.query.profissao)
 })
 
-app.get('/contato', (req, res) => {
-    res.send('Obrigado por entrar em contato com a gente')
+app.post('/', (req, res) => {
+    console.log(req.body)
+    res.send(`O que você me enviou foi: ${req.body.nome} ${req.body.sobrenome}`)
 })
 
 app.listen(3000, () => {
