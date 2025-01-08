ce081b41d83adfde377abbdaf4e1db5cb37e58fb
adrianomts7 <161032614+adrianomts7@users.noreply.github.com>
2025-01-08 13:10:45 -0300

    Criando um banco de dados mongodb e conectando

--- JavaScript/Node/src/controllers/homeControllers.js
+++ JavaScript/Node/src/controllers/homeControllers.js
@@ -1,3 +1,18 @@
+const HomeModel = require('../models/HomeModels')
+
+// Inserindo dados
+// HomeModel.create({
+//     titulo: 'Outra coisa',
+//     descricao: 'outra descrição (Testes)',
+// })
+//     .then(dados => console.log(dados))
+//     .catch(e => console.log(e))
+
+//Ver todos os dados do banco de dados
+HomeModel.find()
+    .then(dados => console.log(dados))
+    .catch(e => console.log(e))
+
 exports.paginaInicial = (req, res) => {
     res.render('index')
     return
--- JavaScript/Node/src/middlewares/middleware.js
+++ JavaScript/Node/src/middlewares/middleware.js
@@ -10,8 +10,3 @@ exports.middlewareGlobal = (req, res, next) => {
     
     next()
 }
-
-exports.outroMiddleware = (req, res, next) => {
-    console.log('Hello world')
-    next()
-}
