4ed85719b7d8a5545360371556bca9db40e2be52
adrianomts7 <161032614+adrianomts7@users.noreply.github.com>
2025-01-09 16:41:22 -0300

    Express Session e Flash

--- JavaScript/Node/server.js
+++ JavaScript/Node/server.js
@@ -11,6 +11,10 @@ mongoose.connect(process.env.CONNECTIONSTRING)
     })    
     .catch( e => console.log(e))
 
+const session = require('express-session')
+const MongoStore = require('connect-mongo');
+const flash = require('connect-flash')
+
 const routes = require('./routes')
 const path = require('path')
 const { middlewareGlobal } = require('./src/middlewares/middleware')
@@ -18,6 +22,21 @@ const { middlewareGlobal } = require('./src/middlewares/middleware')
 app.use(express.urlencoded({ extended: true}))
 
 app.use(express.static(path.resolve(__dirname, 'public')))
+
+const sessionOptions = session({
+    secret: 'bla bla bla',
+    store: MongoStore.create({ mongoUrl: process.env.CONNECTIONSTRING}),
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        // 7 dias em milessimos de segundos, semore em milessimos de segundos
+        maxAge: 1000 * 60 * 60 * 24 * 7,
+        httpOnly: true
+    }
+})
+
+app.use(sessionOptions)
+app.use(flash())
 
 app.set('views', path.resolve(__dirname,'src','views'))
 app.set('view engine', 'ejs')
--- JavaScript/Node/src/controllers/homeControllers.js
+++ JavaScript/Node/src/controllers/homeControllers.js
@@ -9,11 +9,16 @@ const HomeModel = require('../models/HomeModels')
 //     .catch(e => console.log(e))
 
 //Ver todos os dados do banco de dados
-HomeModel.find()
-    .then(dados => console.log(dados))
-    .catch(e => console.log(e))
+// HomeModel.find()
+//     .then(dados => console.log(dados))
+//     .catch(e => console.log(e))
 
 exports.paginaInicial = (req, res) => {
+    // console.log(req.session.usuario)
+    req.flash('erro', 'Erro ao cadastrar cliente')
+    req.flash('sucess', 'Cliente cadastrado com sucesso')
+    req.info('info', 'site em manutenção')
+    console.log(req.flash('erro'), req.flash('sucess'), req.flash('info'))
     res.render('index')
     return
 }
