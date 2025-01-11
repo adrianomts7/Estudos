3fc551e54f090653d52d8b36bcd15722ea7e41c2
adrianomts7 <161032614+adrianomts7@users.noreply.github.com>
2025-01-11 19:20:34 -0300

    helmet e csrf

--- JavaScript/Node/server.js
+++ JavaScript/Node/server.js
@@ -17,10 +17,12 @@ const flash = require('connect-flash')
 
 const routes = require('./routes')
 const path = require('path')
-const { middlewareGlobal } = require('./src/middlewares/middleware')
+const helmet = require('helmet')
+const csrf = require('csurf')
+const { middlewareGlobal, checkCsrfError, csrfMiddleware } = require('./src/middlewares/middleware')
 
+app.use(helmet())
 app.use(express.urlencoded({ extended: true}))
-
 app.use(express.static(path.resolve(__dirname, 'public')))
 
 const sessionOptions = session({
@@ -41,8 +43,11 @@ app.use(flash())
 app.set('views', path.resolve(__dirname,'src','views'))
 app.set('view engine', 'ejs')
 
+app.use(csrf())
 // Nossos próprios middlewares
 app.use(middlewareGlobal)
+app.use(checkCsrfError)
+app.use(csrfMiddleware)
 app.use(routes)
 
 app.on('pronto', () => {
