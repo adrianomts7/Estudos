31806b1bc72ade4ceb36caba3d77c68aee61ac3f
adrianomts7 <161032614+adrianomts7@users.noreply.github.com>
2025-01-15 13:18:08 -0300

    Fazendo verificações do usuario se o email ja existe e criptografando a senha antes de ir pro banco de dados

--- JavaScript/Node/src/models/loginModel.js
+++ JavaScript/Node/src/models/loginModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
+const bcrypt = require('bcrypt')
 
 const LoginSchema = new mongoose.Schema({
     email: { type:String, required: true },
@@ -19,12 +20,23 @@ class Login{
         this.valida()
         if(this.erros.length > 0) return
 
+        await this.userExists()
+
         try{
+            // Criptografando a senha para salvar no schema
+            const salt = bcrypt.genSaltSync()
+            this.body.password = bcrypt.hashSync(this.body.password, salt)
+            
             this.user = await LoginModel.create(this.body)
         }
         catch(e){
             console.log(e)
         }
+    }
+
+    async userExists(){
+        const user = await LoginModel.findOne( {email: this.body.email})
+        if(user) this.erros.push('Usuario já existe')
     }
 
     valida(){
