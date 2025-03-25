931324e0a7964a68787ac184a8fee3d8ec0355fa
adrianomts7 <161032614+adrianomts7@users.noreply.github.com>
2025-03-25 18:27:53 -0300

    Upload de arquivos

--- back-end/app.js
+++ back-end/app.js
@@ -1,10 +1,12 @@
 import dotenv from 'dotenv';
 
 import express from 'express';
+
 import home from './src/routes/home';
 import user from './src/routes/user';
 import token from './src/routes/token';
 import aluno from './src/routes/aluno';
+import foto from './src/routes/foto';
 
 dotenv.config();
 
@@ -25,6 +27,7 @@ class App {
     this.app.use('/user', user);
     this.app.use('/tokens', token);
     this.app.use('/alunos', aluno);
+    this.app.use('/fotos', foto);
   }
 }
 
