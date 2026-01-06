09b4741ebe3988b7fb910b391a9ba05d78acdd10
adrianomts7 <161032614+adrianomts7@users.noreply.github.com>
2026-01-06 18:02:09 -0300

    Padrão de modulo antigo, antes do ES6

--- Curso JavaScript/17-Modern-JS-Modules-Tooling/starter/script.js
+++ Curso JavaScript/17-Modern-JS-Modules-Tooling/starter/script.js
@@ -1,5 +1,7 @@
 // import {addToCart, totalPrice as price, tq} from './shoppingCart.js';
 
+/*
+import shoppingCart from "./shoppingCart";
 console.log('Importing Module')
 // addToCart('Bread', 5);
 // console.log(price, tq);
@@ -35,3 +37,32 @@ console.log(lastPost);
 
 const lastPost2 = await getLastPost();
 console.log(lastPost2);
+*/
+
+const ShoppingCart2 = (function() {
+  const cart = [];
+  const shippingCost = 10;
+  const totalPrice = 237;
+  const totalQuantity = 23;
+
+  const addToCart = function(product, quantity) {
+    cart.push({product, quantity});
+    console.log(`${quantity} ${product} added to cart (sipping cost is ${shippingCost})`);
+  }
+
+  const orderStock = function(product, quantity) {
+    console.log(`${quantity} ${product} ordered from supplier`);
+  }
+
+  return {
+    addToCart, 
+    cart,
+    totalPrice, 
+    totalQuantity 
+  }
+
+})();
+
+ShoppingCart2.addToCart('apple', 4);
+ShoppingCart2.addToCart('pizza', 2);
+console.log(ShoppingCart2);
