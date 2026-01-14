d34b8ee92d3d4bee061e6d32008f58649f9486e4
adrianomts7 <161032614+adrianomts7@users.noreply.github.com>
2026-01-14 18:54:45 -0300

    Implementando resultados que foram  pesquisado na barra lateral

--- Curso JavaScript/18-forkify/starter/src/js/controller.js
+++ Curso JavaScript/18-forkify/starter/src/js/controller.js
@@ -1,6 +1,7 @@
 import * as model from './model.js';
 import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
+import resultsView from './views/resultsView.js';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
@@ -8,12 +9,15 @@ import 'regenerator-runtime/runtime';
 // NEW API URL (instead of the one shown in the video)
 // https://forkify-api.jonas.io
 
-///////////////////////////////////////
+
+if (module.hot) {
+  module.hot.accept();
+}
+
 const controlRecipes = async function() {
   try {
 
-    // const id = window.location.hash.slice(1);
-    const id = '664c8f193e7aa067e94e8823';
+    const id = window.location.hash.slice(1);
 
     if (!id) return;
 
@@ -30,12 +34,15 @@ const controlRecipes = async function() {
 
 const controlSearchResults = async function() {
   try {
+    resultsView.renderSpinner();
+
     const query = searchView.getQuery();
 
     if (!query) return;
 
     await model.loadSearchResults(query);
-    console.log(model.state.search.results);
+
+    resultsView.render(model.state.search.results);
   } catch(err) {
     console.log(err);
   }
