0f7a9512fd2b57a91af4d61c8ffd9658030769f1
adrianomts7 <161032614+adrianomts7@users.noreply.github.com>
2026-02-06 17:35:34 -0300

    Adicionando o react fragment

--- Curso React/03-pizza-menu/src/index.js
+++ Curso React/03-pizza-menu/src/index.js
@@ -75,11 +75,19 @@ function Menu() {
     <main className="menu">
       <h2>Our menu</h2>
       { pizzas.length > 0 ? 
-        <ul className="pizzas">
-          {pizzas.map((pizza) => (
-            <Pizza pizzaObj={pizza} key={pizza.name} />
-          ))}
-          </ul> : <p>We're still working on our menu. Please come back later :)</p> }
+        <>
+          <p>
+            Authentic Italian cuisine. 6 creative dishes to choose from. All from out stone oven, all organic, all delicious 
+          </p>
+          
+          <ul className="pizzas">
+            { pizzas.map((pizza) => (
+              <Pizza pizzaObj={pizza} key={pizza.name} />
+            ))}
+          </ul> 
+        </>
+        : <p>We're still working on our menu. Please come back later :)</p> 
+        }
       {/* <Pizza name="Pizza Spinaci" ingredients="Tomato, mozarella, spinach, and ricotta cheese" photoName="pizzas/spinaci.jpg" price={10} />
       <Pizza name="Pizza Funghi" ingredients="Tomato, mushrooms" photoName='pizzas/funghi.jpg' price={12} /> */}
     </main>
