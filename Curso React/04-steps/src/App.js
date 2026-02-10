96c2a217c422b1e662b26066790cacf0608f25a6
adrianomts7 <161032614+adrianomts7@users.noreply.github.com>
2026-02-10 17:49:36 -0300

    Adicionando nova funcionalidade ao projeto

--- Curso React/04-steps/src/App.js
+++ Curso React/04-steps/src/App.js
@@ -9,6 +9,7 @@ const messages = [
 
 export default function App() {
   const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
 
   function handlePrevious() {
     if (step > 1) setStep(step - 1);
@@ -19,20 +20,26 @@ export default function App() {
   }
 
   return (
-    <div className="steps">
-      <div className="numbers">
-        <div className={step >= 1 ? 'active' : ''}>1</div>
-        <div className={step >= 2 ? 'active' : ''}>2</div>
-        <div className={step >= 3 ? 'active' : ''}>3</div>
-      </div>
+    <>
+      <button className="close" onClick={() => setIsOpen(!isOpen)}>&times;</button>
 
-      <p className="message">Step {step}: {messages[step - 1]}</p>
-
-      <div className="buttons">
-        <button style={styleButtons} onClick={handlePrevious}>Previous</button>
-        <button style={styleButtons} onClick={handleNext}>Next</button>
-      </div>
-
-    </div> 
+      { isOpen && 
+        <div className="steps">
+          <div className="numbers">
+            <div className={step >= 1 ? 'active' : ''}>1</div>
+            <div className={step >= 2 ? 'active' : ''}>2</div>
+            <div className={step >= 3 ? 'active' : ''}>3</div>
+          </div>
+    
+          <p className="message">Step {step}: {messages[step - 1]}</p>
+    
+          <div className="buttons">
+            <button style={styleButtons} onClick={handlePrevious}>Previous</button>
+            <button style={styleButtons} onClick={handleNext}>Next</button>
+          </div>
+    
+        </div> 
+      }
+    </>
   );
 }
