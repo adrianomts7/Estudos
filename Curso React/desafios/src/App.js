5e23a8505079cd666203431a6b7ee12f37156987
adrianomts7 <161032614+adrianomts7@users.noreply.github.com>
2026-02-19 18:09:38 -0300

    Desafio concluido da seção 6

--- Curso React/desafios/src/App.js
+++ Curso React/desafios/src/App.js
@@ -12,36 +12,46 @@ function Counter() {
   const [count, setCount] = useState(0);
   const [step, setStep] = useState(1);
 
-  function previous(valorAtual, valorMinimo, functionInsercao, valorInsercao = 1) {
-    if ((valorAtual - valorInsercao) >= valorMinimo) functionInsercao(v => v - valorInsercao);
-  }
-
-  function next(valorAtual, valorMaximo, functionInsercao, valorInsercao = 1) {
-    if ((valorAtual + valorInsercao) <= valorMaximo) functionInsercao(v => v + valorInsercao);
-  }
-
   const data = new Date();
   data.setDate(data.getDate() + count);
+
+  function handleClickButtonCount(e, decrease = false, step, count) {
+    e.preventDefault();
+    
+    !decrease ? setCount(count += step) : setCount(count -= step);
+  } 
+
+  function handleInputStep(value) {
+    setStep(value);
+  }
+
+  function resetNumbersCountStep() {
+    setCount(0);
+    setStep(1);
+  }
 
   return (
     <div>
       <div className="steps">
-        <button onClick={() => previous(step, 1, setStep)}>-</button>
-        <span>Step: {step}</span>
-        <button onClick={() => next(step, 10, setStep)}>+</button> 
+        <input type="range" min={1} max={10} value={step} step={1} onChange={e => setStep(+e.target.value)} /> <p>{step > 10 ? 10 : step}</p>
       </div>
       
       <div className="counter">
-        <button onClick={() => previous(count, -10, setCount, step)}>-</button>
-        <span>Count: {count}</span>
-        <button onClick={() => next(count, 100, setCount, step)}>+</button>
+        <button onClick={e => handleClickButtonCount(e, true, step, count)}>-</button>
+        <input type="text" value={step} onChange={e => handleInputStep(+e.target.value)} />
+        <button onClick={e => handleClickButtonCount(e, false, step, count)}>+</button>
       </div>
+
+      {
+        count !== 1 || count !== 0 ?
+        <button onClick={resetNumbersCountStep}>Reset</button> :
+        null
+      }
 
       <p>{count} days { count > 0 ? 'from today' :'ago'}   {data.toDateString()}</p>
 
     </div>
   );
 }
-
 
 export default App;
