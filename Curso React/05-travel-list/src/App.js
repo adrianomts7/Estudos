ac11e92e60703c4f97119869604ce3a46e9f11dc
adrianomts7 <161032614+adrianomts7@users.noreply.github.com>
2026-02-20 19:16:20 -0300

    subindo o estado de nivel para que componentes irmãos usem, seja dados ou funções

--- Curso React/05-travel-list/src/App.js
+++ Curso React/05-travel-list/src/App.js
@@ -7,10 +7,16 @@ const initialItems = [
 ];
 
 export default function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    setItems(items => [...items, item]);
+  }
+
   return <div className="app">
     <Logo />
-    <Form />
-    <PackingList />
+    <Form onAddItems={handleAddItems} />
+    <PackingList items={items} />
     <Stats />
   </div>
 };
@@ -19,7 +25,7 @@ function Logo() {
   return <h1>🏝️ Far Away 💼</h1>
 }
 
-function Form() {
+function Form({onAddItems}) {
   const [description, setDescription] = useState('');
   const [quantity, setQuantity] = useState(1);
 
@@ -29,7 +35,7 @@ function Form() {
     if (!description) return;
 
     const newItem = {description, quantity, packed: false, id: Date.now()};
-    console.log(newItem);
+    onAddItems(newItem);
     setDescription('');
     setQuantity(1);
   }
@@ -50,11 +56,11 @@ function Form() {
   )
 }
 
-function PackingList() {
+function PackingList({items}) {
   return(
     <div className="list">
       <ul>
-        { initialItems.map( item => <Item item={item} key={item.id} /> ) }
+        { items.map( item => <Item item={item} key={item.id} /> ) }
       </ul>
     </div>
   );
