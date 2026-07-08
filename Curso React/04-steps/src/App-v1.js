cbbe4816360fc52d35afc2ac0a63a0cd44956825
adrianomts7 <161032614+adrianomts7@users.noreply.github.com>
2026-07-08 17:42:37 -0300

    Aprendendo sobre prop.childen

--- Curso React/04-steps/src/App-v1.js
+++ Curso React/04-steps/src/App-v1.js
@@ -9,7 +9,7 @@ const messages = [
 export default function App() {
   return <div>
     <Steps />
-    {/* <Steps /> */}
+    <Steps />
   </div>
 }
 
@@ -38,17 +38,11 @@ function Steps() {
           </div>
           <p className="message">Step {step}: {messages[step - 1]}</p>
           <div className="buttons">
-            <Button bgColor='#7950f2' textColor='#fff' onClick={handlerPrevious} > <span>👈🏻</span> Previous </Button>
-            <Button bgColor='#7950f2' textColor='#fff' onClick={handlerNext} > Next <span>👉🏻</span> </Button>
+            <button style={{backgroundColor: '#7950f2', color: '#fff'}} onClick={handlerPrevious}>Previous</button>
+            <button style={{backgroundColor: '#7950f2', color: '#fff'}} onClick={handlerNext}>Next</button>
           </div>
         </div>
       }
     </div>
   )
-}
-
-function Button({textColor, bgColor, onClick, children}) {
-  return <button style={{backgroundColor: bgColor, color: textColor}} onClick={onClick}>
-    {children}
-  </button>
 }
