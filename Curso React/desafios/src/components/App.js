a48d43e0ef56e53663dc2c7f07f98ec4e256f000
adrianomts7 <161032614+adrianomts7@users.noreply.github.com>
2026-07-11 19:42:54 -0300

    Concluindo mais um exercicio sobre um formulario de finalização de compra em react

--- Curso React/desafios/src/components/App.js
+++ Curso React/desafios/src/components/App.js
@@ -3,129 +3,119 @@ import { useState } from "react";
 export default function App() {
   return (
     <div>
-      <AberturaChamado />
+      <FormCheckout />
     </div>
   ); 
 }
 
-function AberturaChamado() {
-  const [chamado, setChamado] = useState([]);
+function FormCheckout() {
+  const [dadosCompra, setDadosCompra] = useState({ tipoProduto: '', nome: '', email: '', cep: '', rua: '', numero: '', frequencia: '', termos: false })
+  
+  function submitForm(e) {
+    e.preventDefault(); 
+    let dadosCompraFinal = { tipoProduto: dadosCompra.tipoProduto, nome: dadosCompra.nome, email: dadosCompra.email };
+
+    if (dadosCompra.nome.trim() === "" || dadosCompra.email.trim() === "") return alert('Os campos NOME e EMAIL não podem ficar vázio');
+    
+    if (dadosCompra.tipoProduto === 'fisico') {
+      if (dadosCompra.cep.length !== 8) return alert('CEP inválido');
+      if (dadosCompra.numero.length === 0 || dadosCompra.rua.length === 0) return alert('Os campos de endereço não podem ficar vázio, digite valores válidos!');
+      dadosCompraFinal = {...dadosCompraFinal, cep: dadosCompra.cep, numero: dadosCompra.numero, rua: dadosCompra.rua};
+    }
+
+    if (dadosCompra.tipoProduto === 'digital') {
+      if (!dadosCompra.termos) return alert('Aceite os nossos termos de uso');
+      if (dadosCompra.frequencia.trim() === '') return alert('Escolha a frequência da cobrança');
+      dadosCompraFinal = {...dadosCompraFinal, termos: dadosCompra.termos, frequencia: dadosCompra.frequencia};
+    }
+
+    alert('Compra finalizada');
+    console.log(dadosCompraFinal);
+    dadosCompraFinal = { tipoProduto: dadosCompra.tipoProduto, nome: dadosCompra.nome, email: dadosCompra.email };
+    setDadosCompra({ tipoProduto: '', nome: '', email: '', cep: '', rua: '', numero: '', frequencia: '', termos: false });
+  }
+  
+  function onChange(e) {  
+    const { name, value, type, checked } = e.target;
+  
+    const valorAtualizado = type === 'checkbox' ? checked : value;
+
+    setDadosCompra(produtos => ({...produtos, [name]: valorAtualizado}))
+    console.log(dadosCompra);
+  } 
 
   return (
     <div className='container'>
-      <Header />
-      <Form onChamado={setChamado} />
+      <div className='area__titulo'>
+        <h2>Finalizando Compra</h2>
+        <p>Completo os campos abaixo para finalizar a compra!</p>
+      </div>
+
+      <form onSubmit={submitForm} >
+        <div className='area__input'>
+          <label htmlFor="tipoProduto">Tipo de Produto:</label>
+          <select id='tipoProduto' name="tipoProduto" value={dadosCompra.tipoProduto} required onChange={onChange}>
+            <option value=''>Selecione um tipo de produto</option>
+            <option value='fisico'>Fisico</option>
+            <option value='digital'>Digital</option>
+          </select>
+        </div>
+
+        <div className='area__input'>
+          <label htmlFor='nomeCompleto'>Nome Completo</label>
+          <input type='text' placeholder="Digite o seu nome completo" name='nome' id='nomeCompleto' value={dadosCompra.nome} onChange={onChange} required />
+        </div>
+
+        <div className="area__input">
+          <label htmlFor='email'>Email:</label>
+          <input type='email' name='email' required placeholder="usuario@gmail.com" onChange={onChange} value={dadosCompra.email} />
+        </div>
+
+        { dadosCompra.tipoProduto === 'fisico' && <ProdutoFisico onChange={onChange} dadosCompra={dadosCompra} />}
+        {dadosCompra.tipoProduto === 'digital' && <ProdutoDigital onChange={onChange} dadosCompra={dadosCompra} />}
+
+        <button className='btn__finalizar-compra'>Finalizar Compra</button>
+      </form>
     </div>
   );
 }
 
-function Header() {
+function ProdutoFisico({onChange, dadosCompra}) {
   return (
-    <header>
-      <h1>Abertura de Chamado</h1>
-    </header>
+    <div>
+      <div className='area__input'>
+        <label htmlFor="cep">CEP:</label>
+        <input type='text' name='cep' id='cep' placeholder="00000-000" onChange={onChange} value={dadosCompra.cep} />
+      </div>
+
+      <div className='area__input'>
+        <label htmlFor="rua">Rua:</label>
+        <input type='text' name='rua' id='rua' placeholder="Rua de cima" onChange={onChange} value={dadosCompra.rua} />
+      </div>
+
+      <div className='area__input'>
+        <label htmlFor="numero">Número:</label>
+        <input type='text' name='numero' id='numero' placeholder="7" onChange={onChange} value={dadosCompra.numero} />
+      </div>
+
+    </div>
   );
 }
 
-function Form({ onChamado }) {
-  const [titulo, setTitulo] = useState('');
-  const [categoria, setCategoria] = useState('');
-  const [telefones, setTelefones] = useState([]);
-  const [urgente, setUrgente] = useState(false);
-
-  function handlerSubmit(e) {
-    e.preventDefault();
-
-    if (titulo.length < 1 && telefones.length < 1 && categoria.length < 1) {
-      alert('Os campos abaixos não podem ficar vázio!');
-      return;
-    }
-
-    if (titulo.length < 4) {
-      alert('Digite um titulo de chamado válido!');
-      return;
-    } 
-    
-    if (categoria.length < 2) {
-      alert('Escolha uma categoria de Serviço válida');
-      return;
-    }
-
-    if (telefones.length < 2) {
-      alert('Cadastre pelo menos 2 telefones válidos')
-      return;
-    }
-
-    const chamado = { titulo, categoria, telefones, urgente }
-
-    setTitulo('');
-    setCategoria('');
-    setTelefones([]);
-    setUrgente(false);
-
-    onChamado(chamados => [...chamados, chamado]);
-    alert('Chamado registrado com sucesso!');
-  }
-  
-  return (
-    <form className="form__principal" onSubmit={handlerSubmit}>
-      <div className="area__input">
-        <label htmlFor='titulo'>Titulo:</label>
-        <input type='text' id='titulo' placeholder="Qual a demanda" value={titulo} onChange={e => setTitulo(e.target.value)} />
-      </div>
-
-      <div className="area__input">
-        <label htmlFor='categoria'>Categoria:</label>
-        <select id='categoria' value={categoria} onChange={e => setCategoria(e.target.value)}>
-          <option value=''>Escolha uma opção</option>
-          <option value='hardware'>Hardware (Problema no Computador)</option>
-          <option value='software'>Software (Instalar Sistema Operacional)</option>
-          <option value='redes'>Redes (Wifi caindo)</option>
-          <option value='desenvolvimento'>Desenvolvimento (Criação de Sites/App)</option>
-          <option value='consultoria'>Consultoria (Dúvidas, Orçamentos)</option>
-        </select>
-      </div>
-
-      <div className="area__input-checkbox">
-        <label htmlFor="urgencia">Urgênte:</label>
-        <input type='checkbox' id='urgencia'checked={urgente} onChange={e => setUrgente(e.target.checked)} />
-      </div>
-
-      <Telefones telefones={telefones} onTelefones={setTelefones} />
-
-      <button className='btn__cadastrar-chamado'>Registrar Chamado</button>
-    </form>
-  )
-}
-
-function Telefones({ telefones, onTelefones }) {
-  const [telefone, setTelefone] = useState('');
-
-  function cadastrarTelefone(e) {
-    e.preventDefault(); 
-    if (telefone.length < 5) return;
-
-    onTelefones(telefones => [...telefones, telefone])
-    setTelefone('');
-  }
-
-  function removerTelefone(indice, e) {
-    e.preventDefault();
-
-    onTelefones(telefones => telefones.filter((telefone, i) => i !== indice))
-  }
-
+function ProdutoDigital({onChange, dadosCompra}) {
   return (
     <div>
-      
-      <div className='area__input-telefone'>
-        <label htmlFor='telefone'>Telefone: </label>
-        <input id='telefone' type='text' placeholder="Digite um telefone" value={telefone} onChange={e => setTelefone(e.target.value)} />
-        <button className='btn__cadastro-telefone' onClick={cadastrarTelefone}>Cadastrar Telefone</button>
+      <div className='area__input'>
+        <label htmlFor="frequencia">Frequência da cobrança:</label>
+        <select name='frequencia' onChange={onChange} value={dadosCompra.frequencia}>
+          <option value=''>Escolha a Frequência da cobrança</option>
+          <option value='mensal'>Mensal</option>
+          <option value='anual'>Anual</option>
+        </select> 
       </div>
-
-      <div className="area__telefones">
-          { telefones.map((telefone, i) =><p className='telefone' key={i}>{`Telefone: ${i + 1}`}:{telefone}{ telefones.length > 1 &&<button className="apagar__telefone" onClick={(e) => removerTelefone(i, e)}>Apagar</button>}</p>)}
+      <div className='area__termos-uso'>
+        <span>Aceita os termos de uso:</span>
+        <input type='checkbox' name='termos' checked={dadosCompra.termos} required  onChange={onChange}/>
       </div>
     </div>
   );
