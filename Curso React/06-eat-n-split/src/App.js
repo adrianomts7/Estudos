65d3f9bab24ded688e0520f831ba4569455092b4
adrianomts7 <161032614+adrianomts7@users.noreply.github.com>
2026-07-14 10:27:38 -0300

    Terminando de fazer a parte estatica com os formularios

--- Curso React/06-eat-n-split/src/App.js
+++ Curso React/06-eat-n-split/src/App.js
@@ -23,6 +23,8 @@ export default function App() {
   return <div className='app'>
     <div className='sidebar'>
       <FriendsList />
+      <FormAddFriend />
+      <Button>Add Friend</Button>
     </div>
   </div>
 }
@@ -43,6 +45,47 @@ function Friend({friend}) {
     { friend.balance > 0 && <p className='green'>{friend.name} owes you {Math.abs(friend.balance)}€</p> }
     { friend.balance === 0 && <p>You and {friend.name} are even </p> }
 
-    <button className='button'>Select</button>
+    <Button>Select</Button>
   </li>
+}
+
+function Button({children}) {
+  return <button className='button'>{children}</button>
+}
+
+function FormAddFriend() {
+  return <form className="form-add-frien">
+    <label>🧑‍🤝‍👩🏼 Friend name</label>
+    <input type='text' />
+
+    <label>🌆 Image URL</label>
+    <input type='text' />
+
+    <Button>Add</Button>
+  </form>
+}
+
+function FormSplitBill() {
+  return (
+    <form className="form-split-bill">
+      <h2>Split a bill with X</h2>
+
+      <label>💰 Bill value</label>
+      <input type='text' />
+
+      <label>🧍🏻‍♂️Your expense</label>
+      <input type='text' />
+
+      <label>🧑‍🤝‍👩🏼 X's expense</label>
+      <input type='text' />
+
+      <label>🤑 Who is paying the bill</label>
+      <select>
+        <option value='user'>You</option>
+        <option value='friend'>Friend</option>
+      </select>
+
+      <Button>Split bill</Button>
+    </form>
+  );
 }
