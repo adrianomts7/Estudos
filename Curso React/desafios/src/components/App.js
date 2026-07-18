7b2e7e4e594245c44576b6a6a64e0c755670d865
adrianomts7 <161032614+adrianomts7@users.noreply.github.com>
2026-07-18 12:30:14 -0300

    Implementando algumas melhorias no desafio

--- Curso React/desafios/src/components/App.js
+++ Curso React/desafios/src/components/App.js
@@ -11,18 +11,27 @@ export default function App() {
 
 function GeradorFatura() {
   const [isModal, setIsModal] = useState(false);
-  const [listaClientes, setListaClientes] = useState([]);
+  const [listaClientes, setListaClientes] = useState([{ id: 1, faturas: [{nome: 'Adriano', descricao: 'Manutenção', valor: 20}], valorTotal: 20 }]);
 
-  
-  function novaFatura(fatura, nomeCliente) {
-    setListaClientes(clientes => clientes.length > 0 ? clientes.map(cliente => cliente.nome === nomeCliente ? {...cliente, listaFaturas: [...cliente.listaFaturas, fatura], valorPagar: cliente.valorPagar + fatura.valor} : cliente) : [{nome: nomeCliente, listaFaturas: [fatura], valorPagar: fatura.valor}])
+  const [mostrarMaisInfo, setMostrarMaisInfo] = useState(null);
+
+  function mostrarMaisInfoFatura(idSelecionado) {
+    console.log(mostrarMaisInfo);
+    setMostrarMaisInfo(id => id === idSelecionado ? null : idSelecionado);
   }
-  console.log(listaClientes);
+
+  function novaFatura(fatura, idCliente) {
+    setListaClientes(clientes => [...clientes, { id: idCliente, faturas: [fatura], valorTotal: fatura.valor * fatura.quantidade }]);
+  }
   
+  function adicionarNovaFaturaCliente(idCliente, fatura) {
+    setListaClientes(clientes => clientes.map(cliente => cliente.id === idCliente ? {...cliente, faturas: [...cliente.faturas, fatura], valorTotal: cliente.valor + fatura.valor } : cliente));
+  }
+
   return <div className="container">
     <Header setIsModal={setIsModal} />
     { isModal && <FormFatura setIsModal={setIsModal} isModal={isModal} onAdicionarFatura={novaFatura} />}
-    <ListaFaturas />
+    <ListaFaturas listaClientes={listaClientes} mostrarMaisInfo={mostrarMaisInfo} onMostrarMaisInfo={mostrarMaisInfoFatura} />
   </div>
 }
 
@@ -30,14 +39,14 @@ function Header({setIsModal}) {
   return (
     <header>
       <h1 className="titulo">Gerador de Fatura</h1>
-      <button className="btn-abrir-modal" onClick={() => setIsModal(true)}>Cadastrar Fatura</button>
+      <button className="btn-abrir-modal" onClick={() => setIsModal(true)}>Cadastrar Nova Fatura</button>
     </header>
   );
 
 }
 function FormFatura({setIsModal, isModal, onAdicionarFatura}) {
 
-  const [fatura, setFatura] = useState({nome: '', descricao: '', quantidade: 1, valor: ''});
+  const [fatura, setFatura] = useState({id: crypto.randomUUID() ,nome: '', descricao: '', quantidade: 1, valor: ''});
 
   function pegandoDadosInput(e) {
     const { name, value } = e.target;
@@ -54,7 +63,7 @@ function FormFatura({setIsModal, isModal, onAdicionarFatura}) {
     if (fatura.nome.trim() === "" || fatura.descricao.trim() === "") return alert("Os campos não podem ficar vázio!");
     if (fatura.valor < 0) return alert("Digite um valor de serviço válido");
 
-    onAdicionarFatura(fatura, fatura.nome)
+    onAdicionarFatura(fatura, fatura.id)
     alert("Fatura gerada com sucesso!")
     setIsModal(false);
   }
@@ -93,20 +102,48 @@ function FormFatura({setIsModal, isModal, onAdicionarFatura}) {
   );
 }
 
-function ListaFaturas() {
+function ListaFaturas({ listaClientes, mostrarMaisInfo, onMostrarMaisInfo }) {
   return (
     <ul className="lista-faturas">
-        <Fatura fatura={{ nome: "Adriano", quantidade: 2, descricao: "Limpeza do Computador", valor: 20 }} />
+        { listaClientes.length > 0 ? listaClientes.map(fatura => <Fatura fatura={fatura} key={fatura.id} mostrarMaisInfo={mostrarMaisInfo} onMostrarMaisInfo={onMostrarMaisInfo} />) : <p className="mensagem-fatura-vazia">Nenhuma Fatura de Clientes Registradas!</p> }
     </ul>
   );
 }
 
-function Fatura({fatura}) {
-  return <li>
+function Fatura({fatura, onMostrarMaisInfo, mostrarMaisInfo}) {
+
+  return <li style={mostrarMaisInfo === fatura.id ? { border: '3px solid var(--verde)', padding: '2rem 0.8rem'} : {}}>
     <div>
-      <p>Nome do Cliente: {fatura.nome}</p>
-      <p>Produto Solictado: {fatura.descricao}</p>
-      <p>Quantidade: {fatura.quantidade }</p>
+      <p className="nome-cliente">Nome do Cliente: {fatura.faturas[0].nome}</p>
+      
+      <div className="area-faturas">
+        <p>Quantidade de Produtos Solicitados: <span className="quantidade-produtos">{fatura.faturas.length} </span></p>
+        
+        <p>Valor Total: <span className="valor-total-fatura">R$ {fatura.valorTotal}</span></p>
+      </div>
+      
+      { mostrarMaisInfo === fatura.id && <DivDadosFatura fatura={fatura} /> }
+     
+      <div className="area-butoes-fatura">
+        <button className="btn-mostrar-mais" onClick={() => onMostrarMaisInfo(fatura.id)}>{ mostrarMaisInfo ? "Ocultar Infos" : "Mostrar Infos" }</button>
+        <button className="btn-adicionar-nova-fatura">Adicionar Nova Fatura para {fatura.faturas[0].nome}</button>
+      </div>
     </div>
   </li>
+}
+
+function DivDadosFatura({ fatura }) {
+  return (  
+    <div className="area-dados-fatura">
+      <p className="titulo-descricao-produtos">Descrição dos Produtos Solicitados:</p>
+      { fatura.faturas.map((fatura, i) => 
+        <div className="area-dados" key={fatura.id} >
+           
+          <p>{(i + 1)}º  Descrição: {fatura.descricao}</p>  
+          <p>Quantidade: {fatura.quantidade}</p>
+          <p>Valor: <span className="valor">R$ {fatura.valor}</span></p>
+        </div> 
+      )}
+    </div>
+  )
 }
