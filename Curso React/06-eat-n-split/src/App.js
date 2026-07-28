f80c89d130bb1419e9008ed0c68ac20b232070c8
adrianomts7 <161032614+adrianomts7@users.noreply.github.com>
2026-07-28 18:09:36 -0300

    Utilizando a propriedade key para zerar o estado do componente

--- Curso React/06-eat-n-split/src/App.js
+++ Curso React/06-eat-n-split/src/App.js
@@ -54,7 +54,7 @@ export default function App() {
       <Button onClick={handlerShowAddFriend}>{ showAddFriend ? 'Close' : 'Add Friend'}</Button>
     </div>
     
-    { selectedFriend && <FormSplitBill selectedFriend={selectedFriend} onSplitBill={handleSplitBill} /> }
+    { selectedFriend && <FormSplitBill selectedFriend={selectedFriend} onSplitBill={handleSplitBill}  key={selectedFriend.id} /> }
 
   </div>
 }
