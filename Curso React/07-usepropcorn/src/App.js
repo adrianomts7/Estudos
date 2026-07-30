33fb33117e6701ed3477dac7470f1bf6ace191d3
adrianomts7 <161032614+adrianomts7@users.noreply.github.com>
2026-07-30 18:27:23 -0300

    Criando o estado de carregamento para mostrar ao usuário que os dados estão sendo buscados

--- Curso React/07-usepropcorn/src/App.js
+++ Curso React/07-usepropcorn/src/App.js
@@ -55,13 +55,16 @@ const KEY = '887f39e5'
 export default function App() {  
   const [movies, setMovies] = useState([]);
   const [watched, setWatched] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
   const query = 'interstellar';
 
   useEffect(function() {
     async function fetchMovies() {
+      setIsLoading(true);
       const res = await fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=${query}`);
       const data = await res.json();
       setMovies(data.Search);
+      setIsLoading(false);
     } 
 
     fetchMovies();
@@ -78,7 +81,7 @@ export default function App() {
 
       <Main>
         <Box>
-          <MovieList movies={movies} />
+          { isLoading ? <Loader /> : <MovieList movies={movies} /> }
         </Box>
 
         <Box>
@@ -90,6 +93,9 @@ export default function App() {
   );
 }
 
+function Loader() {
+  return <p className="loader">Loading...</p>
+}
 
 function NavBar({ children }) {  
   return (
