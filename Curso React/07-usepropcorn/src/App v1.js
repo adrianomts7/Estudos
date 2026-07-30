010474b26b94e8467f09d9f9b15b52b44745599e
adrianomts7 <161032614+adrianomts7@users.noreply.github.com>
2026-07-30 17:33:52 -0300

    Forma errada de fazer uma requisição para uma API no react

--- Curso React/07-usepropcorn/src/App v1.js
+++ Curso React/07-usepropcorn/src/App v1.js
@@ -50,14 +50,10 @@ const tempWatchedData = [
 const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
-const KEY = '887f39e5'
-
 export default function App() {  
   const [movies, setMovies] = useState(tempMovieData);
   const [watched, setWatched] = useState(tempWatchedData);
-
-  fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=interstellar`).then(res => res.json()).then(data => console.log(data));
-
+  
   return (
     <>
       <NavBar>
