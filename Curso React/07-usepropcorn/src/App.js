b21b979bd0b5aa98ffd3bacb2049673cf1d860ba
adrianomts7 <161032614+adrianomts7@users.noreply.github.com>
2026-07-30 19:00:03 -0300

    Tratando os erros

--- Curso React/07-usepropcorn/src/App.js
+++ Curso React/07-usepropcorn/src/App.js
@@ -56,18 +56,32 @@ export default function App() {
   const [movies, setMovies] = useState([]);
   const [watched, setWatched] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-  const query = 'interstellar';
+  const [error, setError] = useState("");
+  const query = 'interstelar';
 
   useEffect(function() {
     async function fetchMovies() {
-      setIsLoading(true);
-      const res = await fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=${query}`);
-      const data = await res.json();
-      setMovies(data.Search);
-      setIsLoading(false);
-    } 
+      try { 
+      
+        setIsLoading(true);
+        const res = await fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=${query}`);
 
-    fetchMovies();
+        if (!res.ok) throw new Error("Something went wrong with fetching movies");
+
+        const data = await res.json();
+        
+        if (data.Response === 'False') throw new Error(data.Error);
+
+        setMovies(data.Search);
+      }
+      catch(err) {
+        setError(err.message);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+  fetchMovies();
   }, [])
 
 
@@ -81,7 +95,10 @@ export default function App() {
 
       <Main>
         <Box>
-          { isLoading ? <Loader /> : <MovieList movies={movies} /> }
+          {/* { isLoading ? <Loader /> : <MovieList movies={movies} /> } */}
+          {isLoading && <Loader />}
+          {!isLoading && !error && <MovieList movies={movies} />}
+          {error && <ErrorMessage message={error} />}
         </Box>
 
         <Box>
@@ -95,6 +112,10 @@ export default function App() {
 
 function Loader() {
   return <p className="loader">Loading...</p>
+}
+
+function ErrorMessage({ message }) {
+  return <p className="error"><span>⛔</span> {message}</p>
 }
 
 function NavBar({ children }) {  
@@ -163,7 +184,6 @@ function Box({ children }) {
 }
 
 function MovieList({movies}) {
-
   return (
     <ul className="list">
       {movies?.map((movie) => (
