926d55bf31bb1bbaaafe38e6dfcf9ef11bf946cb
adrianomts7 <161032614+adrianomts7@users.noreply.github.com>
2026-07-30 18:18:49 -0300

    Fazendo a função ficar assíncrona

--- Curso React/07-usepropcorn/src/App.js
+++ Curso React/07-usepropcorn/src/App.js
@@ -53,13 +53,18 @@ const average = (arr) =>
 const KEY = '887f39e5'
 
 export default function App() {  
-  const [movies, setMovies] = useState(tempMovieData);
-  const [watched, setWatched] = useState(tempWatchedData);
+  const [movies, setMovies] = useState([]);
+  const [watched, setWatched] = useState([]);
+  const query = 'interstellar';
 
   useEffect(function() {
-    fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=interstellar`)
-      .then(res => res.json())
-      .then(data => setMovies(data.Search));
+    async function fetchMovies() {
+      const res = await fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=${query}`);
+      const data = await res.json();
+      setMovies(data.Search);
+    } 
+
+    fetchMovies();
   }, [])
 
 
