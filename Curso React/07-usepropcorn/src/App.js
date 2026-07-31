fb3bed5dbf38edda73934a343ab89a7550b2f7d5
adrianomts7 <161032614+adrianomts7@users.noreply.github.com>
2026-07-31 18:38:53 -0300

    Implementação do carregamento do detalhes do filme

--- Curso React/07-usepropcorn/src/App.js
+++ Curso React/07-usepropcorn/src/App.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import StarRating from './StartRating';
 
 const tempMovieData = [
   {
@@ -61,7 +62,7 @@ export default function App() {
   const [selectedId, setSelectedId] = useState(null);
 
   function handleSelectMovie(id) {
-    setSelectedId(selectedId => selectedId === id ? null : id );
+    setSelectedId(selectedId => selectedId === id ? null : id  );
   }
 
   function handleCloseMovie() {
@@ -75,7 +76,7 @@ export default function App() {
           setError("");
           setIsLoading(true);
           const res = await fetch(
-            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
           );
 
           if (!res.ok)
@@ -228,10 +229,55 @@ function Movie({ movie, onSelectMovie }) {
 }
 
 function MovieDetails({ selectedId, onCloseMovie }) {
+  const [movie, setMovie] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+
+  const { Title: title, Year: year, Poster: poster, Runtime: runtime, imdbRating, Plot: plot, Released: released, Actors: actors, Director: director, Genre: genre } = movie; 
+
+  useEffect(function() {
+    async function getMovieDatails() {
+      setIsLoading(true);
+
+      const res = await fetch(
+        `http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
+      );
+      
+      const data = await res.json();
+      setMovie(data);
+      setIsLoading(false);
+    }
+
+    getMovieDatails();
+  }, [selectedId])
+  
   return <div className="details">
-    <button className="btn-back" onClick={onCloseMovie}>&larr;</button>
-    {selectedId}
-    </div>;
+    
+    { isLoading ? <Loader /> : 
+      <>
+        <header>
+          <button className="btn-back" onClick={onCloseMovie}>&larr;</button>
+          <img src={poster} alt={`Poster of ${movie} movie`} />
+          
+          <div className="details-overview">
+            <h2>{title}</h2>
+            <p>{released} &bull; {runtime}</p>
+            <p>{genre}</p>
+            <p><span>⭐</span> {imdbRating} IMDB rating</p>
+          </div>
+
+        </header>
+        <section>
+          <div class="rating">
+            <StarRating maxRating={10} size={24} />
+          </div>
+
+          <p><em>{plot}</em></p>
+          <p>Starring: {actors}</p>
+          <p>Directed by {director}</p>
+        </section>
+      </>
+    }
+  </div>;
 }
 
 function WatchedSummary({ watched }) {
