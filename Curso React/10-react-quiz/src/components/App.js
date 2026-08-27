3de25c46a2f4d70937fb76d169b93b04a33f3653
adrianomts7 <161032614+adrianomts7@users.noreply.github.com>
2026-08-27 19:33:51 -0300

    Adicionando a barra de progresso para o jogador poder ver o seus resultados

--- Curso React/10-react-quiz/src/components/App.js
+++ Curso React/10-react-quiz/src/components/App.js
@@ -6,6 +6,7 @@ import Error from './Error';
 import StartScreen from './StartScreen';
 import Question from './Question';
 import NextButton from './NextButton';
+import Progress from './Progress';
 
 const initialState = {
   questions: [],
@@ -43,9 +44,10 @@ function reducer(state, action) {
 }
 
 export default function App() {
-  const [{ questions, status, index, answer }, dispatch] = useReducer(reducer,initialState);
+  const [{ questions, status, index, answer, points }, dispatch] = useReducer(reducer,initialState);
 
   const numQuestions = questions.length;
+  const maxPossiblePoints = questions.reduce((prev, cur) => prev + cur.points, 0);
 
   useEffect(function() {
     fetch('http://localhost:8000/questions').then(res => res.json()).then(data => dispatch({ type: 'dataReceived', payload: data })).catch(error => dispatch({ type: 'dataFailed' }));
@@ -60,6 +62,7 @@ export default function App() {
         { status === 'ready' && <StartScreen numQuestions={numQuestions} dispatch={dispatch} /> }
         { status === 'active' && (
           <>
+          <Progress index={index} numQuestions={numQuestions} points={points} maxPossiblePoints={maxPossiblePoints} answer={answer} />
           <Question question={questions[index]} dispatch={dispatch} answer={answer} /> 
           <NextButton dispatch={dispatch} answer={answer} />  
           </>
