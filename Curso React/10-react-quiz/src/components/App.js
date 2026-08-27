59c4c9e547f278473a08b8bb04a79deca4d13207
adrianomts7 <161032614+adrianomts7@users.noreply.github.com>
2026-08-27 19:09:52 -0300

    Adicionando a funcionalidade de ir para outra pergunta ao ter respondida a anterior

--- Curso React/10-react-quiz/src/components/App.js
+++ Curso React/10-react-quiz/src/components/App.js
@@ -5,6 +5,7 @@ import Loader from './Loader';
 import Error from './Error';
 import StartScreen from './StartScreen';
 import Question from './Question';
+import NextButton from './NextButton';
 
 const initialState = {
   questions: [],
@@ -32,6 +33,9 @@ function reducer(state, action) {
 
       return { ...state, answer: action.payload, points: action.payload === question.correctOption ? state.points + question.points : state.points };
 
+    case 'nextQuestion':
+      return { ...state, index: state.index + 1, answer: null }
+
     default:
       throw new Error("Action unkonw")
 
@@ -54,7 +58,12 @@ export default function App() {
         { status === 'loading' && <Loader /> }
         { status === 'error' && <Error /> }
         { status === 'ready' && <StartScreen numQuestions={numQuestions} dispatch={dispatch} /> }
-        { status === 'active' && <Question question={questions[index]} dispatch={dispatch} answer={answer} /> }
+        { status === 'active' && (
+          <>
+          <Question question={questions[index]} dispatch={dispatch} answer={answer} /> 
+          <NextButton dispatch={dispatch} answer={answer} />  
+          </>
+        )}
       </Main>
   </div>
 }
