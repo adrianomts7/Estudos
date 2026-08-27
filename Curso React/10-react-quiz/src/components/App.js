e329beeaa18298f47c67e0911adaba0a409caee4
adrianomts7 <161032614+adrianomts7@users.noreply.github.com>
2026-08-27 18:55:45 -0300

    Adicionando a lógica de acrescentar pontos e de mostrar as respostas corretas através das classes

--- Curso React/10-react-quiz/src/components/App.js
+++ Curso React/10-react-quiz/src/components/App.js
@@ -12,6 +12,8 @@ const initialState = {
   // 'loading', 'error', 'ready', 'active', 'finished'
   status: 'loading',
   index: 0,
+  answer: null,
+  points: 0
 }
 
 function reducer(state, action) {
@@ -24,6 +26,11 @@ function reducer(state, action) {
 
     case 'start':
       return { ...state, status: 'active' }
+    
+    case 'newAnswer':
+      const question = state.questions.at(state.index);
+
+      return { ...state, answer: action.payload, points: action.payload === question.correctOption ? state.points + question.points : state.points };
 
     default:
       throw new Error("Action unkonw")
@@ -32,7 +39,7 @@ function reducer(state, action) {
 }
 
 export default function App() {
-  const [{ questions, status, index }, dispatch] = useReducer(reducer,initialState);
+  const [{ questions, status, index, answer }, dispatch] = useReducer(reducer,initialState);
 
   const numQuestions = questions.length;
 
@@ -47,7 +54,7 @@ export default function App() {
         { status === 'loading' && <Loader /> }
         { status === 'error' && <Error /> }
         { status === 'ready' && <StartScreen numQuestions={numQuestions} dispatch={dispatch} /> }
-        { status === 'active' && <Question question={questions[index]} /> }
+        { status === 'active' && <Question question={questions[index]} dispatch={dispatch} answer={answer} /> }
       </Main>
   </div>
 }
