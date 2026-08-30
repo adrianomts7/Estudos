88f34621c9cb0668828f2284eb56357573174997
adrianomts7 <161032614+adrianomts7@users.noreply.github.com>
2026-08-30 16:14:55 -0300

    Adicionando o timer para deixar o jogo mais divertido, PROJETO FINALIZADO da sessão

--- Curso React/10-react-quiz/src/components/App.js
+++ Curso React/10-react-quiz/src/components/App.js
@@ -8,6 +8,10 @@ import Question from './Question';
 import NextButton from './NextButton';
 import Progress from './Progress';
 import FinishScreen from './FinishScreen';
+import Footer from './Footer';
+import Timer from './Timer'
+
+const SEC_PER_QUESTION = 30;
 
 const initialState = {
   questions: [],
@@ -17,7 +21,8 @@ const initialState = {
   index: 0,
   answer: null,
   points: 0,
-  highscore: 0
+  highscore: 0,
+  secondsRemaining: null
 }
 
 function reducer(state, action) {
@@ -29,7 +34,7 @@ function reducer(state, action) {
       return { ...state, status: 'error' }
 
     case 'start':
-      return { ...state, status: 'active' }
+      return { ...state, status: 'active', secondsRemaining: state.questions.length * SEC_PER_QUESTION }
     
     case 'newAnswer':
       const question = state.questions.at(state.index);
@@ -45,6 +50,9 @@ function reducer(state, action) {
     case 'restart':
       return {...initialState, questions: state.questions, status: 'ready'}
 
+    case 'tick':
+      return {...state, secondsRemaining: state.secondsRemaining - 1, status: state.secondsRemaining === 0 ? "finished" : state.status};
+
     default:
       throw new Error("Action unkonw")
 
@@ -52,7 +60,7 @@ function reducer(state, action) {
 }
 
 export default function App() {
-  const [{ questions, status, index, answer, points, highscore }, dispatch] = useReducer(reducer,initialState);
+  const [{ questions, status, index, answer, points, highscore, secondsRemaining }, dispatch] = useReducer(reducer,initialState);
 
   const numQuestions = questions.length;
   const maxPossiblePoints = questions.reduce((prev, cur) => prev + cur.points, 0);
@@ -71,8 +79,11 @@ export default function App() {
         { status === 'active' && (
           <>
           <Progress index={index} numQuestions={numQuestions} points={points} maxPossiblePoints={maxPossiblePoints} answer={answer} />
-          <Question question={questions[index]} dispatch={dispatch} answer={answer} /> 
-          <NextButton dispatch={dispatch} answer={answer} index={index} numQuestion={numQuestions} />  
+          <Question question={questions[index]} dispatch={dispatch} answer={answer} />
+          <Footer>
+            <Timer dispatch={dispatch} secondsRemaining={secondsRemaining} />
+            <NextButton dispatch={dispatch} answer={answer} index={index} numQuestion={numQuestions} />
+          </Footer>
           </>
         )}
         { status === 'finished' && <FinishScreen points={points} maxPossiblePoints={maxPossiblePoints} highscore={highscore} dispatch={dispatch} /> }
