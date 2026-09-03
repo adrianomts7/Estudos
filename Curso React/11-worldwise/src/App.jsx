201314142fcacc994c9722b921b87df7290d4e20
adrianomts7 <161032614+adrianomts7@users.noreply.github.com>
2026-09-03 19:00:20 -0300

    Criando as rotas das páginas do projeto

--- Curso React/11-worldwise/src/App.jsx
+++ Curso React/11-worldwise/src/App.jsx
@@ -1,7 +1,18 @@
-export function App() {
-  const app = 23;
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Product from "./Pages/Product";
+import HomePage from "./Pages/HomePage";
+import Pricing from "./Pages/Pricing";
+import PageNotFound from "./Pages/PageNotFound";
 
+export default function App() {
   return (
-    <div>WorldWise</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
--- Curso React/11-worldwise/vite.config.js
+++ Curso React/11-worldwise/vite.config.js
@@ -1,8 +1,7 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
-import eslintConfig from './eslint.config'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), eslintConfig],
+  plugins: [react()],
 })
