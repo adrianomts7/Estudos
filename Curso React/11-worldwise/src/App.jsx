ef1c78c3738420180277b044f7b3ee745ac17a83
adrianomts7 <161032614+adrianomts7@users.noreply.github.com>
2026-09-15 18:00:15 -0300

    Fazendo a navegação ficar mais intuitiva e determinando rotas padrão

--- Curso React/11-worldwise/src/App.jsx
+++ Curso React/11-worldwise/src/App.jsx
@@ -10,11 +10,15 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route index path="/" element={<HomePage />} />
         <Route path="/product" element={<Product />} />
         <Route path="/pricing" element={<Pricing />} />
         <Route path="/login" element={<Login />} />
-        <Route path="app" element={<AppLayout />} />
+        <Route path="app" element={<AppLayout />}>
+          <Route index path="cities" element={<p>List of Cities</p>} />
+          <Route path="countries" element={<p>Countries</p>} />
+          <Route path="form" element={<p>Form</p>} />
+        </Route>
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
