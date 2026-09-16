11f4bfa7800746473c8c13c34bc892a6e7412cc1
adrianomts7 <161032614+adrianomts7@users.noreply.github.com>
2026-09-16 16:47:31 -0300

    Pegando dados da url usando o useParams

--- Curso React/11-worldwise/src/App.jsx
+++ Curso React/11-worldwise/src/App.jsx
@@ -9,6 +9,7 @@ import PageNotFound from "./Pages/PageNotFound";
 import AppLayout from "./Pages/AppLayout";
 import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
+import City from './components/City';
 
 const BASE_URL = `http://localhost:9000`;
 
@@ -43,6 +44,7 @@ export default function App() {
         <Route path="app" element={<AppLayout />}>
           <Route index element={<CityList cities={cities} isLoading={isLoading} />} />
           <Route  path="cities" element={<CityList cities={cities} isLoading={isLoading} />} />
+          <Route path="cities/:id" element={<City />} />
           <Route path="countries" element={<CountryList cities={cities} isLoading={isLoading} />} />
           <Route path="form" element={<p>Form</p>} />
         </Route>
