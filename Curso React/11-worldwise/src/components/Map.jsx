8ad1a2cea4ab52d8c538f4dfa6e431836a345a2b
adrianomts7 <161032614+adrianomts7@users.noreply.github.com>
2026-09-16 17:06:52 -0300

    fazendo query urls e buscando os dados da url com o useSearchParams dando a opção de editar esses dados na url

--- Curso React/11-worldwise/src/components/Map.jsx
+++ Curso React/11-worldwise/src/components/Map.jsx
@@ -1,6 +1,11 @@
+import { useSearchParams } from 'react-router-dom';
 import style from './Map.module.css';
 
 function Map() {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const lat = searchParams.get('lat');
+  const lng = searchParams.get('lng');
+
   return (
     <div className={style.mapContainer}>
       
