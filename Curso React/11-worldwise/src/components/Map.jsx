34df5c6ef3b92ffc32f9643f4ccffb258e1a6824
adrianomts7 <161032614+adrianomts7@users.noreply.github.com>
2026-09-18 18:31:01 -0300

    Tornando Botões em componentes reutilizaveis e usando useNavigate para retornar ou levar o usuário a algum lugar do site depois de um evento

--- Curso React/11-worldwise/src/components/Map.jsx
+++ Curso React/11-worldwise/src/components/Map.jsx
@@ -1,13 +1,14 @@
-import { useSearchParams } from 'react-router-dom';
+import { useNavigate, useSearchParams } from 'react-router-dom';
 import style from './Map.module.css';
 
 function Map() {
+  const navigate = useNavigate();
   const [searchParams, setSearchParams] = useSearchParams();
   const lat = searchParams.get('lat');
   const lng = searchParams.get('lng');
 
   return (
-    <div className={style.mapContainer}>
+    <div className={style.mapContainer} onClick={() => navigate("form")}>
       
     </div>
   )
